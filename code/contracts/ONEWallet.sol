bd92c9cc8eacb4ac4dfcd4073b6d0b9dd7c651e2
Aaron Li <aaron@qokka.ai>
2021-06-09 03:15:31 -0700

    new ONEWallet contract

---
+++ code/contracts/ONEWallet.sol
@@ -0,0 +1,159 @@
+pragma solidity ^0.8.4;
+pragma experimental ABIEncoderV2;
+
+contract ONEWallet {
+    bytes32 root; // Note: @ivan brought up a good point in reducing this to 16-bytes so hash of two consecutive nodes can be done in a single word (to save gas and reduce blockchain clutter). Let's not worry about that for now and re-evalaute this later.
+    uint8 height; // including the root. e.g. for a tree with 4 leaves, the height is 3.
+    uint8 interval; // otp interval in seconds, default is 30
+    uint32 t0; // starting time block (effectiveTime (in ms) / interval)
+    uint32 lifespan;  // in number of block (e.g. 1 block per [interval] seconds)
+
+    // mutable
+    address payable lastResortAddress;
+    uint256 dailyLimit; // uint128 is sufficient, but uint256 is more efficient since EVM works with 32-byte words.
+    uint256 spentToday;
+    uint32 lastTransferDay;
+
+    struct Commit {
+        bytes32 hash;
+        uint32 timestamp;
+    }
+
+    Commit[] commits;
+
+    uint256[64] ______gap;
+
+    constructor(bytes32 root_, uint8 height_, uint8 interval_, uint32 t0_, uint32 lifespan_,
+        address payable lastResortAddress_, uint256 dailyLimit_)
+    {
+        root = root_;
+        height = height_;
+        interval = interval_;
+        t0 = t0_;
+        lifespan_ = interval_;
+        lastResortAddress = lastResortAddress_;
+        dailyLimit = dailyLimit_;
+    }
+
+    function commit(bytes32 hash) external
+    {
+        _cleanupCommits();
+        uint32 ct = _findCommit(hash);
+        require(ct == - 1, "Commit already exists");
+        Commit memory nc = Commit(hash, uint32(block.timestamp));
+        commits.push(nc);
+    }
+
+    function revealTransfer(bytes32[] calldata neighbors, uint32 index, byte32 eotp, address dest, uint256 amount) external
+    isValidIndex(index)
+    isCorrectProof(neighbors, index, eotp)
+    returns (bool)
+    {
+        bytes packed = bytes.concat(neighbors, bytes32(bytes4(index)), eotp, bytes32(bytes20(dest)), bytes32(amount));
+        require(_canReveal(packed), "Failed to reveal transfer");
+        uint32 day = block.timestamp / 86400;
+        if (day > lastTransferDay) {
+            spentToday = 0;
+            lastTransferDay = day;
+        }
+        if (spentToday + amount > dailyLimit) {
+            return false;
+        }
+        bool success = dest.send(amount);
+        if (!success) {
+            return false;
+        }
+        spentToday += amount;
+        return true;
+    }
+
+    function revealRecovery(bytes32[] calldata neighbors, uint32 index, byte32 eotp) external
+    isValidIndex(index)
+    isCorrectProof(neighbors, index, eotp)
+    returns (bool)
+    {
+        bytes packed = bytes.concat(neighbors, bytes32(bytes4(index)), eotp);
+        require(_canReveal(packed), "Failed to reveal recovery");
+        if (lastResortAddress == address(0)) {
+            return false;
+        }
+        _drain();
+        return true;
+    }
+
+    function _drain() internal {
+        require(lastResortAddress != address(0), "Last resort address is not provided");
+        lastResortAddress.transfer(address(this).balance);
+    }
+
+    modifier isValidIndex(uint32 index)
+    {
+        uint32 counter = block.timestamp / interval - t0;
+        require(counter == index, "Code has incorrect timestamp");
+        _;
+    }
+
+    modifier isCorrectProof(bytes32[] calldata neighbors, uint32 index, byte32 eotp)
+    {
+        byte32 h = sha256(eotp);
+        for (uint8 i = 0; i < height - 1; i++) {
+            if (index & 0x01 == 0x01) {
+                h = sha256(bytes.concat(neighbors[i], h));
+                //                h = sha256(abi.encoderPacked(neighbors[i], h));
+            } else {
+                h = sha256(bytes.concat(h, neighbors[i]));
+                //                h = sha256(abi.encoderPacked(h, neighbors[i]));
+            }
+            index >>= 1;
+        }
+        require(root == h, "Proof is incorrect");
+        _;
+    }
+
+    function _findCommit(bytes32 hash) view internal returns (uint32)
+    {
+        if (hash == "") {
+            return - 1;
+        }
+        for (uint8 i = 0; i < commits.length; i++) {
+            Commit storage c = commits[i];
+            if (c.hash == hash) {
+                return c.timestamp;
+            }
+        }
+        return - 1;
+    }
+
+    // simple mechanism to prevent commits grow unbounded, if an attacker decides to spam commits (at their own expense)
+    function _cleanupCommits() internal
+    {
+        uint8 index = 0;
+        uint32 bt = uint32(block.timestamp);
+        for (uint8 i = 0; i < commits.length; i++) {
+            Commit c = commits[i];
+            if (c.timestamp >= bt - 30) {
+                index = i;
+                break;
+            }
+        }
+        uint8 len = commits.length;
+        for (uint8 i = index; i < len; i++) {
+            commits[i - index] = commits[i];
+        }
+        commits.length = len - index;
+    }
+
+    function _isRevealTimely(uint32 commitTime) view internal returns (bool)
+    {
+        return block.timestamp - commitTime < 30;
+    }
+
+    function _canReveal(bytes32 expectedHash) view internal returns (bool)
+    {
+        byte32 h = sha256(expectedHash);
+        uint32 ct = _findCommit(h);
+        require(ct > 0, "Cannot find commit for this transaction");
+        require(_isRevealTimely(ct), "Reveal is too late. Please re-commit");
+        return true;
+    }
+}
