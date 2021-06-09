596e69c9ed085e2d1f96c550af6302787910a830
Aaron Li <aaron@qokka.ai>
2021-06-09 05:01:03 -0700

    fix all compile errors

--- code/contracts/ONEWallet.sol
+++ code/contracts/ONEWallet.sol
@@ -1,3 +1,4 @@
+// SPDX-License-Identifier: Apache-2.0
 pragma solidity ^0.8.4;
 pragma experimental ABIEncoderV2;
 
@@ -40,20 +41,22 @@ contract ONEWallet {
     {
         require(!commitLocked, "Cleanup in progress. Queue is temporarily locked. Please resubmit.");
         uint32 ct = _findCommit(hash);
-        require(ct == - 1, "Commit already exists");
+        require(ct == 0, "Commit already exists");
         Commit memory nc = Commit(hash, uint32(block.timestamp));
         commits.push(nc);
     }
 
-    function revealTransfer(bytes32[] calldata neighbors, uint32 index, byte32 eotp, address dest, uint256 amount) external
+    function revealTransfer(bytes32[] calldata neighbors, uint32 index, bytes32 eotp, address payable dest, uint256 amount) external
     isValidIndex(index)
     isCorrectProof(neighbors, index, eotp)
     returns (bool)
     {
         _cleanupCommits();
-        bytes packed = bytes.concat(neighbors, bytes32(bytes4(index)), eotp, bytes32(bytes20(dest)), bytes32(amount));
+        bytes memory packedNeighbors = _packBytes32Array(neighbors);
+        bytes memory packed = bytes.concat(packedNeighbors,
+            bytes32(bytes4(index)), eotp, bytes32(bytes20(address(dest))), bytes32(amount));
         require(_canReveal(packed), "Failed to reveal transfer");
-        uint32 day = block.timestamp / 86400;
+        uint32 day = uint32(block.timestamp) / 86400;
         if (day > lastTransferDay) {
             spentToday = 0;
             lastTransferDay = day;
@@ -69,12 +72,13 @@ contract ONEWallet {
         return true;
     }
 
-    function revealRecovery(bytes32[] calldata neighbors, uint32 index, byte32 eotp) external
+    function revealRecovery(bytes32[] calldata neighbors, uint32 index, bytes32 eotp) external
     isValidIndex(index)
     isCorrectProof(neighbors, index, eotp)
     returns (bool)
     {
-        bytes packed = bytes.concat(neighbors, bytes32(bytes4(index)), eotp);
+        bytes memory packedNeighbors = _packBytes32Array(neighbors);
+        bytes memory packed = bytes.concat(packedNeighbors, bytes32(bytes4(index)), eotp);
         require(_canReveal(packed), "Failed to reveal recovery");
         if (lastResortAddress == address(0)) {
             return false;
@@ -90,14 +94,14 @@ contract ONEWallet {
 
     modifier isValidIndex(uint32 index)
     {
-        uint32 counter = block.timestamp / interval - t0;
+        uint32 counter = uint32(block.timestamp) / interval - t0;
         require(counter == index, "Code has incorrect timestamp");
         _;
     }
 
-    modifier isCorrectProof(bytes32[] calldata neighbors, uint32 index, byte32 eotp)
+    modifier isCorrectProof(bytes32[] calldata neighbors, uint32 index, bytes32 eotp)
     {
-        byte32 h = sha256(eotp);
+        bytes32 h = sha256(bytes.concat(eotp));
         for (uint8 i = 0; i < height - 1; i++) {
             if (index & 0x01 == 0x01) {
                 h = sha256(bytes.concat(neighbors[i], h));
@@ -115,7 +119,7 @@ contract ONEWallet {
     function _findCommit(bytes32 hash) view internal returns (uint32)
     {
         if (hash == "") {
-            return - 1;
+            return 0;
         }
         for (uint8 i = 0; i < commits.length; i++) {
             Commit storage c = commits[i];
@@ -123,7 +127,7 @@ contract ONEWallet {
                 return c.timestamp;
             }
         }
-        return - 1;
+        return 0;
     }
 
     // simple mechanism to prevent commits grow unbounded, if an attacker decides to spam commits (at their own expense)
@@ -133,17 +137,19 @@ contract ONEWallet {
         uint8 index = 0;
         uint32 bt = uint32(block.timestamp);
         for (uint8 i = 0; i < commits.length; i++) {
-            Commit c = commits[i];
+            Commit storage c = commits[i];
             if (c.timestamp >= bt - 30) {
                 index = i;
                 break;
             }
         }
-        uint8 len = commits.length;
+        uint32 len = uint32(commits.length);
         for (uint8 i = index; i < len; i++) {
             commits[i - index] = commits[i];
         }
-        commits.length = len - index;
+        for (uint8 i = 0; i < index; i++) {
+            commits.pop();
+        }
         commitLocked = false;
     }
 
@@ -152,13 +158,25 @@ contract ONEWallet {
         return block.timestamp - commitTime < 30;
     }
 
-    function _canReveal(bytes32 expectedHash) view internal returns (bool)
+    function _canReveal(bytes memory packedArgs) view internal returns (bool)
     {
-        byte32 h = keccak256(expectedHash);
+        bytes32 h = keccak256(bytes.concat(packedArgs));
         uint32 ct = _findCommit(h);
         require(ct > 0, "Cannot find commit for this transaction");
         // this should not happen (since old commit should be cleaned up already), but let's check just in case
         require(_isRevealTimely(ct), "Reveal is too late. Please re-commit");
         return true;
+    }
+
+    // same output as abi.encodePacked(x), but the behavior of abi.encodePacked is sort of uncertain
+    function _packBytes32Array(bytes32[] calldata x) pure internal returns (bytes memory)
+    {
+        bytes memory r = new bytes(x.length * 32);
+        for (uint8 i = 0; i < x.length; i++) {
+            for (uint8 j = 0; j < 32; j++) {
+                r[i * 32 + j] = x[i][j];
+            }
+        }
+        return r;
     }
 }
