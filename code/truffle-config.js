2b0e32c42913ab8d16d2790e3935884500fc5c8e
Aaron Li <aaron@qokka.ai>
2021-06-10 11:03:18 -0700

    ditto

--- code/truffle-config.js
+++ code/truffle-config.js
@@ -57,8 +57,7 @@ module.exports = {
   // Configure your compilers
   compilers: {
     solc: {
-      version: '0.7.6', // Fetch exact version from solc-bin (default: truffle's version)
-      // docker: true,        // Use "0.5.1" you've installed locally with docker (default: false)
+      version: '0.8.4',
       // settings: {          // See the solidity docs for advice about optimization and evmVersion
       //  optimizer: {
       //    enabled: false,
