897af01b02288bd70a29e7b50881cb11719b068c
Aaron Li <aaron@qokka.ai>
2021-06-10 11:03:34 -0700

    reduce migration script

--- code/migrations/01_libraries.js
+++ code/migrations/01_libraries.js
@@ -1,9 +1,15 @@
-var DailyLimit = artifacts.require('DailyLimit')
-var Guardians = artifacts.require('Guardians')
-var Recovery = artifacts.require('Recovery')
+const ONEWallet = artifacts.require('ONEWallet')
 
 module.exports = function (deployer) {
-  deployer.deploy(DailyLimit)
-  deployer.deploy(Guardians)
-  deployer.deploy(Recovery)
+//   deployer.deploy(ONEWallet)
 }
+
+// var DailyLimit = artifacts.require('DailyLimit')
+// var Guardians = artifacts.require('Guardians')
+// var Recovery = artifacts.require('Recovery')
+//
+// module.exports = function (deployer) {
+//   deployer.deploy(DailyLimit)
+//   deployer.deploy(Guardians)
+//   deployer.deploy(Recovery)
+// }
