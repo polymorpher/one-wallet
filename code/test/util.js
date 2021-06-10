de305fb9736c87e21bcbbb7c852e923cc23c3bcf
Aaron Li <aaron@qokka.ai>
2021-06-10 11:03:46 -0700

    test core util

---
+++ code/test/util.js
@@ -0,0 +1,68 @@
+const ONEWalletLib = require('../lib/onewallet')
+const ONEWallet = artifacts.require('ONEWallet')
+const base32 = require('hi-base32')
+// t0, lifespan, maxOperationsPerInterval, lastResortAddress, dailyLimit
+const INTERVAL = 30000
+const createWallet = async ({ effectiveTime, duration, maxOperationsPerInterval, lastResortAddress, dailyLimit }) => {
+  const otpSeed = base32.encode('0xdeadbeef1234567890')
+  effectiveTime = Math.floor(effectiveTime / INTERVAL) * INTERVAL
+  const { root, leaves, layers, maxOperationsPerInterval: slotSize } = ONEWalletLib.computeMerkleTree({
+    otpSeed, effectiveTime, maxOperationsPerInterval, duration })
+  const height = layers.length
+  const t0 = effectiveTime / INTERVAL
+  const lifespan = duration / INTERVAL
+  const interval = INTERVAL / 1000
+  // constructor(bytes32 root_, uint8 height_, uint8 interval_, uint32 t0_, uint32 lifespan_, uint8 maxOperationsPerInterval_,
+  //   address payable lastResortAddress_, uint256 dailyLimit_)
+  const wallet = await ONEWallet.new(root, height, interval, t0, lifespan, slotSize, lastResortAddress, dailyLimit)
+
+  return {
+    wallet, // smart contract
+    root, // uint8array
+    client: {
+      leaves, // uint8array packed altogether
+      layers, // uint8array[] each layer is packed uint8array
+    },
+    contract: {
+      slotSize, // maxOperationsPerInterval
+      t0, // starting index
+      lifespan, // number of indices before the contract expires
+      interval // seconds
+    }
+  }
+}
+
+const getClient = () => {
+  return new Promise((resolve, reject) => {
+    web3.eth.getNodeInfo((err, res) => {
+      if (err !== null) return reject(err)
+      return resolve(res)
+    })
+  })
+}
+
+const increaseTime = async (seconds) => {
+  const client = await getClient()
+  if (client.indexOf('TestRPC') === -1) {
+    console.warning('Client is not ganache-cli and cannot forward time')
+    return
+  }
+
+  await web3.currentProvider.send({
+    jsonrpc: '2.0',
+    method: 'evm_increaseTime',
+    params: [seconds],
+    id: 0,
+  })
+
+  await web3.currentProvider.send({
+    jsonrpc: '2.0',
+    method: 'evm_mine',
+    params: [],
+    id: 0,
+  })
+}
+module.exports = {
+  increaseTime,
+  createWallet
+}
