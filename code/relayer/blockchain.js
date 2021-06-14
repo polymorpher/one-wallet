7966f532b819070ea1cdd01bf82e502bb05f393f
Aaron Li <aaron@qokka.ai>
2021-06-13 23:26:27 -0700

    /new api fully working

--- code/relayer/blockchain.js
+++ code/relayer/blockchain.js
@@ -2,7 +2,7 @@ const config = require('./config')
 const contract = require('@truffle/contract')
 // const { TruffleProvider } = require('@harmony-js/core')
 const ONEWallet = require('../build/contracts/ONEWallet.json')
-const PrivateKeyProvider = require('truffle-privatekey-provider')
+const HDWalletProvider = require('@truffle/hdwallet-provider')
 
 let providers = {}; let contracts = {}; let networks = []
 
@@ -21,20 +21,29 @@ let providers = {}; let contracts = {}; let networks = []
 const init = () => {
   Object.keys(config.networks).forEach(k => {
     const n = config.networks[k]
+    console.log(n)
     if (n.key) {
       networks.push(k)
       if (k.startsWith('eth')) {
-        providers[k] = new PrivateKeyProvider(n.key, n.url)
+        providers[k] = new HDWalletProvider({ privateKeys: [n.key], providerOrUrl: n.url })
       } else {
         // providers[k] = new HarmonyProvider(n.key, n.url, n.chainId) // TODO: try and debug later
-        providers[k] = new PrivateKeyProvider(n.key, n.url)
+        providers[k] = new HDWalletProvider({ privateKeys: [n.key], providerOrUrl: n.url })
       }
     }
   })
   Object.keys(providers).forEach(k => {
     const c = contract(ONEWallet)
     c.setProvider(providers[k])
+    c.defaults({
+      from: providers[k].getAddress(0)
+    })
     contracts[k] = c
+  })
+  console.log('init complete:', {
+    networks,
+    providers,
+    contracts
   })
 }
 
--- code/relayer/routes/index.js
+++ code/relayer/routes/index.js
@@ -1,20 +1,22 @@
-const _ = require('lodash')
+const { values, mapValues } = require('lodash')
 const config = require('../config')
 const express = require('express')
 const router = express.Router()
 const { StatusCodes } = require('http-status-codes')
 const blockchain = require('../blockchain')
+const BN = require('bn.js')
+
 router.use((req, res, next) => {
   const s = req.header('X-ONEWALLET-RELAYER-SECRET')
   if (s !== config.secret) {
-    return res.status(StatusCodes.UNAUTHORIZED).json({ error: `Invalid secret: ${s}` })
+    return res.status(StatusCodes.UNAUTHORIZED).json({ error: `Invalid X-ONEWALLET-RELAYER-SECRET: ${s}` })
   }
   const network = req.header('X-NETWORK')
   if (!network) {
     return res.status(StatusCodes.BAD_REQUEST).json({ error: 'Must selected network in header X-NETWORK' })
   }
   if (!blockchain.getNetworks().includes(network)) {
-    return res.status(StatusCodes.NOT_IMPLEMENTED).json({ error: `Unsupported network${network}` })
+    return res.status(StatusCodes.NOT_IMPLEMENTED).json({ error: `Unsupported network ${network}` })
   }
   req.network = network
   req.contract = blockchain.getContract(network)
@@ -23,16 +25,25 @@ router.use((req, res, next) => {
 })
 
 router.post('/new', async (req, res) => {
-  const { root, height, interval, t0, lifespan, slotSize, lastResortAddress, dailyLimit } = req.body
+  let { root, height, interval, t0, lifespan, slotSize, lastResortAddress, dailyLimit } = req.body
+  // root is hex string, 32 bytes
+  height = parseInt(height)
+  interval = parseInt(interval)
+  t0 = parseInt(t0)
+  lifespan = parseInt(lifespan)
+  slotSize = parseInt(slotSize)
+  // lastResortAddress is hex string, 20 bytes
+  // dailyLimit is a BN in string form
   if (config.debug || config.verbose) {
     console.log(`[/new] `, { root, height, interval, t0, lifespan, slotSize, lastResortAddress, dailyLimit })
   }
-  const params = [root, height, interval, t0, lifespan, slotSize, lastResortAddress, dailyLimit]
-  if (params.includes(undefined) || params.includes(null)) {
+  const params = mapValues({ root, height, interval, t0, lifespan, slotSize, lastResortAddress, dailyLimit }, e => e === undefined ? null : e)
+
+  if (values(params).includes(undefined) || values(params).includes(null)) {
     return res.status(StatusCodes.BAD_REQUEST).json({ error: 'Some parameters are missing', params })
   }
   try {
-    const wallet = await req.contract.new(root, height, interval, t0, lifespan, slotSize, lastResortAddress, dailyLimit)
+    const wallet = await req.contract.new(root, height, interval, t0, lifespan, slotSize, lastResortAddress, new BN(dailyLimit, 10))
     return res.json({ success: true, address: wallet.address })
   } catch (ex) {
     console.error(ex)
