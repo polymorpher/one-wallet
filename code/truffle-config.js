6939d6b154b6a5e373149666a4bf73edb9b3f576
Aaron Li <aaron@qokka.ai>
2021-06-13 23:25:51 -0700

    fix wallet provider

--- code/truffle-config.js
+++ code/truffle-config.js
@@ -1,43 +1,37 @@
 const config = require('./config')
-const PrivateKeyProvider = require('truffle-privatekey-provider')
+const HDWalletProvider = require('@truffle/hdwallet-provider')
 // const { TruffleProvider } = require('@harmony-js/core') // TODO
+
+const BuildProvider = (key, url) => new HDWalletProvider({
+  privateKeys: [key],
+  providerOrUrl: url,
+  numberOfAddresses: 1,
+  shareNonce: true,
+  derivationPath: "m/44'/1'/0'/0/"
+})
 
 module.exports = {
   networks: {
     dev: {
-      // host: '127.0.0.1',
-      // port: 7545,
       network_id: '*', // Match any network id
       gas: config.gasLimit,
       gasPrice: config.gasPrice,
       websockets: true,
-      provider: config.eth.ganache.key && new PrivateKeyProvider(
-        config.eth.ganache.key,
-        config.eth.ganache.url
-      ),
+      provider: () => config.eth.ganache.key && BuildProvider(config.eth.ganache.key, config.eth.ganache.url)
     },
     rinkeby: {
       network_id: '4',
       gas: config.gasLimit,
       gasPrice: config.gasPrice,
-      provider: config.eth.rinkeby.key && new PrivateKeyProvider(
-        config.eth.rinkeby.key,
-        config.eth.rinkeby.url
-      ),
+      provider: () => config.eth.rinkeby.key && BuildProvider(config.eth.rinkeby.key, config.eth.rinkeby.url)
     },
     'harmony-testnet': {
-      provider: config.harmony.testnet.key && new PrivateKeyProvider(
-        config.harmony.testnet.key,
-        config.harmony.testnet.url,
-      ),
+      provider: () => config.harmony.testnet.key && BuildProvider(config.harmony.testnet.key, config.harmony.testnet.url),
       network_id: '1666700000',
       gas: config.gasLimit
     },
     'harmony-mainnet': {
-      provider: config.harmony.mainnet.key && new PrivateKeyProvider(
-        config.harmony.mainnet.key,
-        config.harmony.mainnet.url,
-      ),
+      provider: () => config.harmony.mainnet.key && BuildProvider(config.harmony.mainnet.key, config.harmony.mainnet.url),
       network_id: '1666600000',
       gas: config.gasLimit
     }
