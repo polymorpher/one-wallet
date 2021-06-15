cc539e2037e7e035466c6fd12853423cdcb788c4
Aaron Li <aaron@qokka.ai>
2021-06-15 13:14:17 -0700

    fix bad exports from wallet state module

--- code/client/src/state/modules/wallet/index.js
+++ code/client/src/state/modules/wallet/index.js
@@ -1,6 +1,6 @@
 import reducer from './reducers'
 
-export { default as userSagas } from './sagas'
-export { default as userActions } from './actions'
+export { default as walletSagas } from './sagas'
+export { default as walletActions } from './actions'
 
 export default reducer
