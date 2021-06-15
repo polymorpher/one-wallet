70ef6ad2150a8ade3c7f46f5461d72ace4f6b562
Aaron Li <aaron@qokka.ai>
2021-06-15 13:15:04 -0700

    sider menu

--- code/client/src/components/SiderMenu.jsx
+++ code/client/src/components/SiderMenu.jsx
@@ -1,1 +1,21 @@
-export default {}
+import React from 'react'
+import { Layout, Image, Menu } from 'antd'
+import { PlusCircleOutlined, UnorderedListOutlined, HistoryOutlined } from '@ant-design/icons'
+import HarmonyLogo from '../assets/harmony.svg'
+
+const SiderMenu = () => {
+  return (
+    <Layout.Sider>
+      {/* <Image src='/assets/harmony.svg' /> */}
+      <Image src={HarmonyLogo} />
+      <Menu theme='dark' mode='inline' defaultSelectedKeys={['1']}>
+        <Menu.Item key='1' icon={PlusCircleOutlined}>Create</Menu.Item>
+        <Menu.Item key='2' icon={UnorderedListOutlined}>List</Menu.Item>
+        <Menu.Item key='3' icon={HistoryOutlined}>Restore</Menu.Item>
+      </Menu>
+
+    </Layout.Sider>
+  )
+}
+
+export default SiderMenu
