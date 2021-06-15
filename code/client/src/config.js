8ca84a166d483df492ba9bae13d13a157eb1a478
Aaron Li <aaron@qokka.ai>
2021-06-15 13:13:40 -0700

    config versioned

--- code/client/src/config.js
+++ code/client/src/config.js
@@ -1,6 +1,7 @@
 export default {
   appId: 'ONEWallet',
   appName: 'ONE Wallet',
+  version: 'v0.0.1',
   defaults: {
     relayer: process.env.REACT_APP_RELAYER || 'https://dev.hiddenstate.xyz',
     network: process.env.NETWORK || 'eth-ganache',
