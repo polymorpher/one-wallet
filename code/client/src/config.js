a791ccae660afd105915565038e29c4fddf5b955
Aaron Li <aaron@qokka.ai>
2021-06-15 23:12:50 -0700

    default relayers and networks

--- code/client/src/config.js
+++ code/client/src/config.js
@@ -3,7 +3,32 @@ export default {
   appName: 'ONE Wallet',
   version: 'v0.0.1',
   defaults: {
-    relayer: process.env.REACT_APP_RELAYER || 'https://dev.hiddenstate.xyz',
-    network: process.env.NETWORK || 'eth-ganache',
+    relayer: process.env.REACT_APP_RELAYER || 'dev',
+    network: process.env.REACT_APP_NETWORK || 'eth-ganache',
+  },
+  networks: {
+    'eth-ganache': {
+      name: 'Ethereum Ganache',
+      url: 'http://127.0.0.1:7545'
+    },
+    'harmony-mainnet': {
+      name: 'Harmony Mainnet',
+      url: 'https://api.s0.t.hmny.io'
+    },
+    'harmony-testnet': {
+      name: 'Harmony Testnet',
+      url: 'https://api.s0.b.hmny.io'
+    }
+  },
+  relayers: {
+    dev: {
+      name: 'Local Development',
+      // url: 'https://127.0.0.1:8443'
+      url: 'https://dev.hiddenstate.xyz'
+    },
+    hiddenstate: {
+      name: 'ONE Wallet Test Relayer',
+      url: 'https://relayer.onewallet.hiddenstate.xyz'
+    }
   }
 }
