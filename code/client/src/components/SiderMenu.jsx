72069f4e5bc1596ea844f3c4ceb0a1e66b2962f1
Aaron Li <aaron@qokka.ai>
2021-06-15 20:07:56 -0700

    sidermenu

--- code/client/src/components/SiderMenu.jsx
+++ code/client/src/components/SiderMenu.jsx
@@ -1,17 +1,34 @@
 import React from 'react'
+import { useHistory, useRouteMatch } from 'react-router'
 import { Layout, Image, Menu } from 'antd'
 import { PlusCircleOutlined, UnorderedListOutlined, HistoryOutlined } from '@ant-design/icons'
 import HarmonyLogo from '../assets/harmony.svg'
+import config from '../config'
+import Paths from '../constants/paths'
+import styled from 'styled-components'
+
+const Text = styled.p`
+  color: #fafafa;
+  text-align: center;
+  margin-bottom: 32px;
+`
 
 const SiderMenu = ({ ...args }) => {
+  const history = useHistory()
+  const match = useRouteMatch('/:action')
+  const { action } = match.params
+  const nav = ({ key }) => {
+    history.push(Paths[key])
+  }
   return (
     <Layout.Sider {...args}>
       {/* <Image src='/assets/harmony.svg' /> */}
-      <Image src={HarmonyLogo} />
-      <Menu theme='dark' mode='inline' defaultSelectedKeys={['1']}>
-        <Menu.Item key='1' icon={PlusCircleOutlined}>Create</Menu.Item>
-        <Menu.Item key='2' icon={UnorderedListOutlined}>List</Menu.Item>
-        <Menu.Item key='3' icon={HistoryOutlined}>Restore</Menu.Item>
+      <Image preview={false} src={HarmonyLogo} style={{ padding: 32 }} />
+      <Text>{config.appName} {config.version}</Text>
+      <Menu theme='dark' mode='inline' onClick={nav} selectedKeys={[action]}>
+        <Menu.Item key='create' icon={<PlusCircleOutlined />}>Create</Menu.Item>
+        <Menu.Item key='list' icon={<UnorderedListOutlined />}>List</Menu.Item>
+        <Menu.Item key='restore' icon={<HistoryOutlined />}>Restore</Menu.Item>
       </Menu>
 
     </Layout.Sider>
