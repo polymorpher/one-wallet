9b3a444117dac1c944572fd07e7f1d81a6b9ba3c
Aaron Li <aaron@qokka.ai>
2021-06-15 20:08:00 -0700

    header

--- code/client/src/components/WalletHeader.jsx
+++ code/client/src/components/WalletHeader.jsx
@@ -1,6 +1,23 @@
 import React from 'react'
+// import styled from 'styled-components'
+import { PageHeader } from 'antd'
+import { useRouteMatch } from 'react-router'
+import { titleCase } from 'title-case'
+import { useSelector } from 'react-redux'
+
 const WalletHeader = () => {
-  return <></>
+  const match = useRouteMatch('/:action')
+  const { action } = match.params
+  const address = useSelector(state => state.wallet.selected)
+
+  return (
+    <PageHeader
+      style={{ background: '#ffffff' }}
+      onBack={() => null}
+      title={titleCase(action || '')}
+      subTitle={address || ''}
+    />
+  )
 }
 
 export default WalletHeader
