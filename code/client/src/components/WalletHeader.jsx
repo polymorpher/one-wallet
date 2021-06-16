f3e1bc74fa53f361c4843a33050d4ee5ec75660c
Aaron Li <aaron@qokka.ai>
2021-06-15 23:13:29 -0700

    Polished header

--- code/client/src/components/WalletHeader.jsx
+++ code/client/src/components/WalletHeader.jsx
@@ -1,9 +1,59 @@
-import React from 'react'
-// import styled from 'styled-components'
-import { PageHeader } from 'antd'
+import React, { useState } from 'react'
+import styled from 'styled-components'
+import { PageHeader, Select } from 'antd'
 import { useRouteMatch } from 'react-router'
 import { titleCase } from 'title-case'
-import { useSelector } from 'react-redux'
+import { useSelector, useDispatch } from 'react-redux'
+import walletActions from '../state/modules/wallet/actions'
+import config from '../config'
+
+const SelectorLabel = styled.span`
+  margin: 16px;
+`
+const NetworkSelector = () => {
+  const networkId = useSelector(state => state.wallet.network)
+  const dispatch = useDispatch()
+  const networks = config.networks
+  const onChange = (v) => {
+    dispatch(walletActions.setNetwork(v))
+  }
+  return (
+    <>
+      <SelectorLabel>Network</SelectorLabel>
+      <Select style={{ width: 160 }} bordered={false} value={networkId} onChange={onChange}>
+        {Object.keys(networks).map(k => {
+          return <Select.Option key={k} value={k}>{networks[k].name} </Select.Option>
+        })}
+
+      </Select>
+    </>
+  )
+}
+
+const RelayerSelector = () => {
+  const relayer = useSelector(state => state.wallet.relayer)
+  const [input, setInput] = useState('')
+  const dispatch = useDispatch()
+  const relayers = config.relayers
+  const onChange = (v) => {
+    dispatch(walletActions.setRelayer(v))
+  }
+  return (
+    <>
+      <SelectorLabel>Relayer</SelectorLabel>
+      <Select
+        style={{ width: 200 }} dropdownMatchSelectWidth bordered={false} showSearch onChange={onChange}
+        value={relayer}
+        onSearch={(v) => setInput(v)}
+      >
+        {Object.keys(relayers).map(k => {
+          return <Select.Option key={k} value={k}>{relayers[k].name} </Select.Option>
+        })}
+        {input && <Select.Option key={input} value={input}>{input}</Select.Option>}
+      </Select>
+    </>
+  )
+}
 
 const WalletHeader = () => {
   const match = useRouteMatch('/:action')
@@ -16,6 +66,10 @@ const WalletHeader = () => {
       onBack={() => null}
       title={titleCase(action || '')}
       subTitle={address || ''}
+      extra={[
+        <RelayerSelector key='relayer' />,
+        <NetworkSelector key='network' />
+      ]}
     />
   )
 }
