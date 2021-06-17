5d3860296093578d7fd909cd8f2f684a819d54f3
Aaron Li <aaron@qokka.ai>
2021-06-17 04:51:45 -0700

    Show stub

--- code/client/src/pages/Show.jsx
+++ code/client/src/pages/Show.jsx
@@ -1,6 +1,24 @@
-import React from 'react'
-
+import React, { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { useHistory, useRouteMatch } from 'react-router'
+import Paths from '../constants/paths'
+import walletActions from '../state/modules/wallet/actions'
 const Show = () => {
+  const history = useHistory()
+  const dispatch = useDispatch()
+  const wallets = useSelector(state => state.wallet.wallets)
+  const match = useRouteMatch(Paths.show)
+  const { address } = match ? match.params : {}
+  const selectedAddress = useSelector(state => state.wallet.selected)
+  useEffect(() => {
+    if (!wallets[address]) {
+      return history.push(Paths.wallets)
+    }
+    if (address && (address !== selectedAddress)) {
+      dispatch(walletActions.selectWallet(address))
+    }
+  }, [])
+
   return <>Show</>
 }
 export default Show
