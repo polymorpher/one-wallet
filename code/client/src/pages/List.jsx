7ea0a4ad112eb9bc3621f0c5bb28ba3c010ec0e4
Aaron Li <aaron@qokka.ai>
2021-06-17 04:51:02 -0700

    improve List page

--- code/client/src/pages/List.jsx
+++ code/client/src/pages/List.jsx
@@ -3,8 +3,10 @@ import ONEUtil from '../../../lib/util'
 import { useSelector, useDispatch } from 'react-redux'
 import walletActions from '../state/modules/wallet/actions'
 import { values, sum } from 'lodash'
-import { Card, Row, Space, Typography, Col } from 'antd'
+import { Card, Row, Space, Typography, message } from 'antd'
 import util from '../util'
+import { useHistory } from 'react-router'
+import Paths from '../constants/paths'
 const { Text, Link, Title } = Typography
 
 const computeBalance = (balance, price) => {
@@ -16,7 +18,8 @@ const computeBalance = (balance, price) => {
 }
 
 const WalletCard = ({ wallet }) => {
-  const { address, name, effectiveTime, lifespan } = wallet
+  const history = useHistory()
+  const { address, name } = wallet
   const dispatch = useDispatch()
   const balances = useSelector(state => state.wallet.balances)
   const balance = balances[address]
@@ -29,13 +32,20 @@ const WalletCard = ({ wallet }) => {
 
   return (
     <Card
+      onClick={() => history.push(Paths.showAddress(address))}
       title={<Title level={2}>{name}</Title>}
       hoverable style={{ borderRadius: 20, width: 320, height: 196 }}
       extra={<Space style={{ alignItems: 'baseline' }}><Title level={3} style={{ marginBottom: 0 }}>{formatted}</Title><Text type='secondary'>ONE</Text></Space>}
     >
       <Space direction='vertical' size='large'>
         <Space><Title level={4}>≈ ${fiatFormatted}</Title><Text type='secondary'>USD</Text></Space>
-        <Text ellipsis={{ tooltip: address }} style={{ width: 196 }}>{address}</Text>
+        <Text
+          ellipsis={{ tooltip: address }} style={{ width: 196 }} onClick={() => {
+            navigator.clipboard.writeText(address)
+            message.info('Copied address to clipboard')
+          }}
+        >{address}
+        </Text>
       </Space>
     </Card>
   )
@@ -47,8 +57,6 @@ const List = () => {
   const price = useSelector(state => state.wallet.price)
   const totalBalance = sum(values(balances))
   const { formatted, fiatFormatted } = computeBalance(totalBalance, price)
-  const numWallets = Object.keys(wallets).length
-  const noun = numWallets > 1 ? 'wallets' : 'wallet'
 
   return (
     <>
