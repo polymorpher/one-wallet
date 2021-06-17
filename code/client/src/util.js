3175614e2c1289e9d796b69346c79923dee6e521
Aaron Li <aaron@qokka.ai>
2021-06-17 03:34:32 -0700

    format number util

--- code/client/src/util.js
+++ code/client/src/util.js
@@ -12,5 +12,16 @@ export default {
     } else {
       message.error(`Failed to create wallet on blockchain. Error: ${error}`)
     }
+  },
+
+  formatNumber: (number, maxPrecision) => {
+    maxPrecision = maxPrecision || 5
+    number = parseFloat(number)
+    if (number < 10 ** (-maxPrecision)) {
+      return '0'
+    }
+    const order = Math.ceil(Math.log10(Math.max(number, 1)))
+    const digits = Math.max(0, maxPrecision - order)
+    return number.toFixed(digits)
   }
 }
