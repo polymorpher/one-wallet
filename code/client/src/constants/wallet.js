bb3a8791a5c485753ff11421edcf857d7268024a
Aaron Li <aaron@qokka.ai>
2021-06-16 18:37:42 -0700

    client: wallet constants

---
+++ code/client/src/constants/wallet.js
@@ -0,0 +1,7 @@
+export default {
+  interval: 30 * 1000,
+  defaultDuration: 3600 * 1000 * 24 * 364,
+  defaultDailyLimit: 1000, // ONEs
+  minDuration: 3600 * 1000 * 24 * 120,
+  maxDuration: 3600 * 1000 * 24 * 364 * 2,
+}
