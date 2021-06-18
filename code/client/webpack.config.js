5298a771ad5a70ac3f3ad99694f806a40c492085
Aaron Li <aaron@qokka.ai>
2021-06-18 06:29:25 -0700

    webpack env

--- code/client/webpack.config.js
+++ code/client/webpack.config.js
@@ -84,6 +84,7 @@ module.exports = {
     }
   },
   plugins: [
+    new webpack.EnvironmentPlugin(['PUBLIC_URL', 'REACT_APP_NETWORK', 'REACT_APP_RELAYER', 'REACT_APP_RELAYER_SECRET']),
     new webpack.ProvidePlugin({
       Buffer: ['buffer', 'Buffer'],
     }),
