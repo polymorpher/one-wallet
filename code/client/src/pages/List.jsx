c88126ab31bc9292fdc020c632208726802ed780
Aaron Li <aaron@qokka.ai>
2021-06-28 06:12:19 -0700

    fix bug with showing balance sum from all networks

--- code/client/src/pages/List.jsx
+++ code/client/src/pages/List.jsx
@@ -49,7 +49,8 @@ const List = () => {
   const balances = useSelector(state => state.wallet.balances)
   const price = useSelector(state => state.wallet.price)
   const network = useSelector(state => state.wallet.network)
-  const totalBalance = values(balances).reduce((a, b) => a.add(new BN(b, 10)), new BN(0)).toString()
+  const totalBalance = Object.keys(balances).filter(a => wallets[a].network === network).map(a => balances[a])
+    .reduce((a, b) => a.add(new BN(b, 10)), new BN(0)).toString()
   const { formatted, fiatFormatted } = util.computeBalance(totalBalance, price)
   return (
     <>
