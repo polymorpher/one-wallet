d1e2120c7046eb1999412d468351f2e5ce70a6b0
Aaron Li <aaron@qokka.ai>
2021-07-06 00:14:28 -0700

    use SmartFlow everywhere

--- code/client/src/pages/Show.jsx
+++ code/client/src/pages/Show.jsx
@@ -5,7 +5,6 @@ import Paths from '../constants/paths'
 import WalletConstants from '../constants/wallet'
 import walletActions from '../state/modules/wallet/actions'
 import util from '../util'
-import ONEUtil from '../../../lib/util'
 import ONE from '../../../lib/onewallet'
 import api from '../api'
 import { message, Space, Row, Col, Typography, Button, Steps, Popconfirm, Tooltip } from 'antd'
@@ -28,7 +27,7 @@ import config from '../config'
 import OtpBox from '../components/OtpBox'
 import { getAddress } from '@harmony-js/crypto'
 import { handleAddressError } from '../handler'
-import { SmartFlows, Chaining } from '../api/flow'
+import { SmartFlows, Chaining, EotpBuilders } from '../api/flow'
 const { Title, Text, Link } = Typography
 const { Step } = Steps
 const TallRow = styled(Row)`
@@ -173,6 +172,17 @@ const Show = () => {
     message.error(`Failed to finalize transaction. Trying ${numAttemptsRemaining} more time`)
   }
 
+  const onRevealSuccess = (txId) => {
+    setStage(3)
+    if (config.networks[network].explorer) {
+      const link = config.networks[network].explorer.replaceAll('{{txId}}', txId)
+      message.success(<Text>Done! View transaction <Link href={link} target='_blank' rel='noreferrer'>{util.ellipsisAddress(txId)}</Link></Text>, 10)
+    } else {
+      message.success(<Text>Transfer completed! Copy transaction id: <Text copyable={{ text: txId }}>{util.ellipsisAddress(txId)} </Text></Text>, 10)
+    }
+    setOtpInput('')
+  }
+
   const doSend = async () => {
     const { otp, dest, amount } = prepareValidation() || {}
     if (!otp || !dest) return
@@ -192,181 +202,54 @@ const Show = () => {
       onRevealError,
       onRevealAttemptFailed,
       onRevealSuccess: (txId) => {
-        setStage(3)
-        if (config.networks[network].explorer) {
-          const link = config.networks[network].explorer.replaceAll('{{txId}}', txId)
-          message.success(<Text>Done! View transaction <Link href={link} target='_blank' rel='noreferrer'>{util.ellipsisAddress(txId)}</Link></Text>, 10)
-        } else {
-          message.success(<Text>Transfer completed! Copy transaction id: <Text copyable={{ text: txId }}>{util.ellipsisAddress(txId)} </Text></Text>, 10)
-        }
-        setOtpInput('')
+        onRevealSuccess(txId)
         Chaining.refreshBalance(dispatch, intersection(Object.keys(wallets), [dest, address]))
       }
     })
   }
 
   const doRecovery = async () => {
-    const { hseed, root, effectiveTime } = wallet
-    const layers = await storage.getItem(root)
-    if (!layers) {
-      message.error('Cannot find pre-computed proofs for this wallet. Storage might be corrupted. Please restore the wallet from Google Authenticator.')
-      return
-    }
-    const index = ONEUtil.timeToIndex({ effectiveTime })
-    const leaf = layers[0].subarray(index * 32, index * 32 + 32).slice()
-    const { eotp } = ONE.bruteforceEOTP({ hseed: ONEUtil.hexToBytes(hseed), leaf })
-    if (!eotp) {
-      message.error('Pre-computed proofs are inconsistent. Recovery cannot proceed')
-      return
-    }
-    const neighbors = ONE.selectMerkleNeighbors({ layers, index })
-    const neighbor = neighbors[0]
-    const { hash: commitHash } = ONE.computeRecoveryHash({
-      neighbor,
-      index,
-      eotp,
+    SmartFlows.commitReveal({
+      wallet,
+      eotpBuilder: EotpBuilders.recovery,
+      commitHashGenerator: ONE.computeRecoveryHash,
+      beforeCommit: () => setStage(1),
+      afterCommit: () => setStage(2),
+      onCommitError,
+      onCommitFailure,
+      revealAPI: api.relayer.revealRecovery,
+      onRevealFailure,
+      onRevealError,
+      onRevealAttemptFailed,
+      onRevealSuccess
     })
-    setStage(1)
-    try {
-      const { success, error } = await api.relayer.commit({ address, hash: ONEUtil.hexString(commitHash) })
-      if (!success) {
-        message.error(`Cannot commit recovery transaction. Error: ${error}`)
-        setStage(0)
-        return
-      }
-    } catch (ex) {
-      console.error(ex)
-      message.error('Failed to commit the recovery transaction. Error: ' + ex.toString())
-      setStage(0)
-      return
-    }
-    setStage(2)
-    let numAttemptsRemaining = WalletConstants.maxTransferAttempts - 1
-    const tryReveal = () => setTimeout(async () => {
-      try {
-        // TODO: should reveal only when commit is confirmed and viewable on-chain. This should be fixed before releasing it to 1k+ users
-        const { success, txId, error } = await api.relayer.revealRecovery({
-          neighbors: neighbors.map(n => ONEUtil.hexString(n)),
-          index,
-          eotp: ONEUtil.hexString(eotp),
-          address
-        })
-        if (!success) {
-          if (error.includes('Cannot find commit')) {
-            message.error(`Network busy. Trying ${numAttemptsRemaining} more time`)
-            numAttemptsRemaining -= 1
-            return tryReveal()
-          }
-          message.error(`Transaction Failed: ${error}`)
-          setStage(0)
-          return
-        }
-        setStage(3)
-        if (config.networks[network].explorer) {
-          const link = config.networks[network].explorer.replaceAll('{{txId}}', txId)
-          message.success(<Text>Done! View transaction <Link href={link} target='_blank' rel='noreferrer'>{util.ellipsisAddress(txId)}</Link></Text>, 10)
-        } else {
-          message.success(<Text>Recovery is completed! Copy transaction id: <Text copyable={{ text: txId }}>{util.ellipsisAddress(txId)} </Text></Text>, 10)
-        }
-      } catch (ex) {
-        console.trace(ex)
-        if (numAttemptsRemaining <= 0) {
-          message.error('Failed to finalize recovery. Please try again later.')
-          setStage(0)
-          return
-        }
-        message.error(`Failed to finalize recovery. Trying ${numAttemptsRemaining} more time`)
-        numAttemptsRemaining -= 1
-        tryReveal()
-      }
-    }, 5000)
-    tryReveal()
   }
 
   const doSetRecoveryAddress = async () => {
-    const { hseed, root, effectiveTime } = wallet
-    const layers = await storage.getItem(root)
-    if (!layers) {
-      message.error('Cannot find pre-computed proofs for this wallet. Storage might be corrupted. Please restore the wallet from Google Authenticator.')
-      return
-    }
-    const parsedOtp = parseInt(otpInput)
-    if (!isInteger(parsedOtp) || !(parsedOtp < 1000000)) {
-      message.error('Google Authenticator code is not valid')
-      return
-    }
-    const normalizedTransferTo = util.safeExec(util.normalizedAddress, [transferTo], handleAddressError)
-    if (!normalizedTransferTo) {
-      return
-    }
-    const otp = ONEUtil.encodeNumericalOtp(parsedOtp)
-    const eotp = ONE.computeEOTP({ otp, hseed: ONEUtil.hexToBytes(hseed) })
-    const index = ONEUtil.timeToIndex({ effectiveTime })
-    const neighbors = ONE.selectMerkleNeighbors({ layers, index })
-    const neighbor = neighbors[0]
+    const { otp, dest } = prepareValidation({ checkAmount: false }) || {}
+    if (!otp || !dest) return
 
-    const { hash: commitHash } = ONE.computeSetRecoveryAddressHash({
-      neighbor,
-      index,
-      eotp,
-      address: normalizedTransferTo,
-    })
-    setStage(1)
-    try {
-      const { success, error } = await api.relayer.commit({ address, hash: ONEUtil.hexString(commitHash) })
-      if (!success) {
-        message.error(`Cannot commit. Reason: ${error}`)
-        setStage(0)
-        return
-      }
-    } catch (ex) {
-      console.error(ex)
-      message.error('Failed to commit. Error: ' + ex.toString())
-      setStage(0)
-      return
-    }
-    setStage(2)
-    let numAttemptsRemaining = WalletConstants.maxTransferAttempts - 1
-    const tryReveal = () => setTimeout(async () => {
-      try {
-        // TODO: should reveal only when commit is confirmed and viewable on-chain. This should be fixed before releasing it to 1k+ users
-        // TODO: Prevent transfer more than maxOperationsPerInterval per interval (30 seconds)
-        const { success, error } = await api.relayer.revealSetRecoveryAddress({
-          neighbors: neighbors.map(n => ONEUtil.hexString(n)),
-          index,
-          eotp: ONEUtil.hexString(eotp),
-          lastResortAddress: normalizedTransferTo,
-          address
-        })
-        if (!success) {
-          message.error(`Transaction Failed: ${error}`)
-          // TODO: use error codes later
-          if (error.includes('Cannot find commit')) {
-            message.error(`Network busy. Trying ${numAttemptsRemaining} more time`)
-            numAttemptsRemaining -= 1
-            return tryReveal()
-          }
-          setStage(0)
-          setOtpInput('')
-          return
-        }
-        setStage(3)
+    SmartFlows.commitReveal({
+      wallet,
+      otp,
+      commitHashGenerator: ONE.computeSetRecoveryAddressHash,
+      commitHashArgs: { address: dest },
+      beforeCommit: () => setStage(1),
+      afterCommit: () => setStage(2),
+      onCommitError,
+      onCommitFailure,
+      revealAPI: api.relayer.revealSetRecoveryAddress,
+      revealArgs: { lastResortAddress: dest },
+      onRevealFailure,
+      onRevealError,
+      onRevealAttemptFailed,
+      onRevealSuccess: (txId) => {
+        onRevealSuccess(txId)
         message.success(`Recovery address is set to ${transferTo}`)
         dispatch(walletActions.fetchWallet({ address }))
         showStats()
-      } catch (ex) {
-        console.trace(ex)
-        if (numAttemptsRemaining <= 0) {
-          message.error('Failed to finalize setting recovery address.')
-          setStage(0)
-          return
-        }
-        message.error(`Failed to finalize setting recovery address. Trying ${numAttemptsRemaining} more time`)
-        numAttemptsRemaining -= 1
-        tryReveal()
       }
-    }, WalletConstants.checkCommitInterval)
-    tryReveal()
+    })
   }
 
   // UI Rendering below
@@ -507,8 +390,8 @@ const Show = () => {
         {stage > 0 && (
           <Row style={{ marginTop: 32 }}>
             <Steps current={stage}>
-              <Step title='Prepare' description='Preparing transfer' />
-              <Step title='Commit' description='Committing transaction' />
+              <Step title='Prepare' description='Preparing signature' />
+              <Step title='Commit' description='Locking-in operation' />
               <Step title='Finalize' description='Submitting proofs' />
             </Steps>
           </Row>)}
@@ -533,9 +416,9 @@ const Show = () => {
             {stage > 0 && (
               <Row style={{ marginTop: 32 }}>
                 <Steps current={stage}>
-                  <Step title='Prepare' description='Preparing recovery proofs' />
-                  <Step title='Commit' description='Submitting recovery signature to blockchain' />
-                  <Step title='Finalize' description='Showing proofs to complete transaction' />
+                  <Step title='Prepare' description='Preparing signature' />
+                  <Step title='Commit' description='Locking-in operation' />
+                  <Step title='Finalize' description='Submitting proofs' />
                 </Steps>
               </Row>)}
           </>}
@@ -579,8 +462,8 @@ const Show = () => {
         {stage > 0 && (
           <Row style={{ marginTop: 32 }}>
             <Steps current={stage}>
-              <Step title='Prepare' description='Preparing operation' />
-              <Step title='Commit' description='Committing operation' />
+              <Step title='Prepare' description='Preparing signature' />
+              <Step title='Commit' description='Locking-in operation' />
               <Step title='Finalize' description='Submitting proofs' />
             </Steps>
           </Row>)}
