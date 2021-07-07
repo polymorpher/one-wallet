8fad4527f9ca65b5b2578936b465affcd194c994
Aaron Li <aaron@qokka.ai>
2021-07-07 04:22:54 -0700

    cli worker

---
+++ code/cli/src/ONEWalletWorker.js
@@ -0,0 +1,38 @@
+const ONE = require('../../lib/onewallet')
+const { parentPort } = require('worker_threads')
+
+parentPort.once('message', ({ seed, effectiveTime, duration, slotSize, interval } = {}) => {
+  if (!seed) {
+    console.log('Worker: received event but it has no valid data')
+    return
+  }
+  // console.log('Worker: generating wallet:', { seed, effectiveTime, duration, slotSize, interval })
+
+  const {
+    hseed,
+    leaves,
+    root,
+    layers,
+    maxOperationsPerInterval,
+  } = ONE.computeMerkleTree({
+    otpSeed: seed,
+    effectiveTime,
+    duration,
+    maxOperationsPerInterval: slotSize,
+    otpInterval: interval,
+    progressObserver: (current, total, stage) => {
+      parentPort.postMessage({ status: 'working', current: current, total: total, stage })
+    }
+  })
+  // console.log('Worker: done')
+  parentPort.postMessage({
+    status: 'done',
+    result: {
+      hseed,
+      leaves,
+      root,
+      layers,
+      maxOperationsPerInterval,
+    }
+  }, layers.map(e => e.buffer))
+})
