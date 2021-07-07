3536e0e38b1459ba1512056f8a08417f77e80c18
Aaron Li <aaron@qokka.ai>
2021-07-07 04:22:27 -0700

    cli config

---
+++ code/cli/constants.js
@@ -0,0 +1,11 @@
+module.exports = {
+  interval: 30 * 1000,
+  defaultDuration: 3600 * 1000 * 24 * 364,
+  defaultDailyLimit: 1000, // ONEs
+  minDuration: 3600 * 1000 * 24 * 120,
+  maxDuration: 3600 * 1000 * 24 * 364 * 2,
+  maxTransferAttempts: 3,
+  checkCommitInterval: 5000,
+  fetchBalanceFrequency: 15000,
+  fetchDelaysAfterTransfer: [0, 2000, 5000]
+}
