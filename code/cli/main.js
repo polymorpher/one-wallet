2a73bb8a98503400607dd9a295620e3331cb891b
Aaron Li <aaron@qokka.ai>
2021-07-07 04:22:21 -0700

    cli command parser

---
+++ code/cli/main.js
@@ -0,0 +1,33 @@
+const config = require('./config')
+const cmd = require('./cmd')
+const store = require('./src/store')
+const importJSX = require('import-jsx')
+const NewWallet = importJSX('./src/scan')
+const rl = require('readline').createInterface({
+  input: process.stdin,
+  output: process.stdout
+})
+
+const isCommand = (command) => cmd._[0] === command
+
+// const hang = () => {
+//   rl.question('Type "exit" and press enter to exit: ', response => {
+//     if (response === 'exit') {
+//       console.log(response)
+//       rl.close()
+//       process.exit(0)
+//     }
+//     hang()
+//   })
+// }
+
+async function main () {
+  await store.ensureDir()
+  if (isCommand('scan')) {
+    NewWallet({ network: cmd.network })
+  }
+  // hang()
+  // process.exit(0)
+}
+
+main()
