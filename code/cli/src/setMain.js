e173bbadb90351fb463e741cd6297b42fb37cbeb
Aaron Li <aaron@qokka.ai>
2021-07-11 08:24:55 -0700

    cli main: update state using blockchain wallet info

--- code/cli/src/setMain.js
+++ code/cli/src/setMain.js
@@ -1,9 +1,9 @@
 import React, { useEffect, useState } from 'react'
 import { render, Text } from 'ink'
 import Info from './info'
-import { loadWalletState, saveToMain } from './store'
+import { loadWalletState, overrideWalletState, saveToMain } from './store'
 import { HarmonyAddress } from '@harmony-js/crypto'
-
+import { api } from '../../lib/api'
 const SetMain = ({ name, address }) => {
   const [error, setError] = useState()
   const [wallet, setWallet] = useState()
@@ -15,9 +15,15 @@ const SetMain = ({ name, address }) => {
       if (error) {
         return setError(error)
       }
-
-      setWallet(wallet)
-      await saveToMain({ address: new HarmonyAddress(wallet.address).bech32, name: wallet.name })
+      const blockchainWallet = await api.blockchain.getWallet({ address: wallet.address })
+      const updatedWallet = { ...wallet, ...blockchainWallet }
+      setWallet(updatedWallet)
+      const { file } = await saveToMain({ address: new HarmonyAddress(wallet.address).bech32, name: wallet.name })
+      const { error: error2 } = await overrideWalletState({ filename: file, state: updatedWallet })
+      if (error2) {
+        return setError(error2)
+      }
+      process.exit(0)
     })()
   }, [])
   return (
