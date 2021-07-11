73c9818d9f603768bfbf9413a674cb28cec0d4f5
Aaron Li <aaron@qokka.ai>
2021-07-11 02:41:52 -0700

    cleanup cli constants

--- code/cli/src/constants.js
+++ code/cli/src/constants.js
@@ -1,11 +1,7 @@
 export default {
   interval: 30 * 1000,
   defaultDuration: 3600 * 1000 * 24 * 364,
-  defaultDailyLimit: 1000, // ONEs
-  minDuration: 3600 * 1000 * 24 * 120,
-  maxDuration: 3600 * 1000 * 24 * 364 * 2,
+  defaultDailyLimit: process.env.DAILY_LIMIT | 1000, // ONEs
   maxTransferAttempts: 3,
   checkCommitInterval: 5000,
-  fetchBalanceFrequency: 15000,
-  fetchDelaysAfterTransfer: [0, 2000, 5000]
 }
