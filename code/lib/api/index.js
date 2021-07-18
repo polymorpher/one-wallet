e7f3aaa12a61b85dd8c4da9c02389e5a3871abae
Aaron Li <aaron@qokka.ai>
2021-07-17 21:46:05 -0700

    api with token operations

--- code/lib/api/index.js
+++ code/lib/api/index.js
@@ -5,8 +5,15 @@ const contract = require('@truffle/contract')
 const { TruffleProvider } = require('@harmony-js/core')
 const Web3 = require('web3')
 const ONEWalletContract = require('../../build/contracts/ONEWallet.json')
+const IERC20 = require('../../build/contracts/IERC20.json')
+const IERC20Metadata = require('../../build/contracts/IERC20Metadata.json')
+const IERC721 = require('../../build/contracts/IERC721.json')
+const IERC721Metadata = require('../../build/contracts/IERC721Metadata.json')
+const IERC1155 = require('../../build/contracts/IERC1155.json')
+const IERC1155MetadataURI = require('../../build/contracts/IERC1155MetadataURI.json')
 const BN = require('bn.js')
 const ONEUtil = require('../util')
+const ONEConstants = require('../constants')
 
 const apiConfig = {
   relayer: config.defaults.relayer,
@@ -44,9 +51,15 @@ const initAPI = (store) => {
   })
 }
 
-const providers = {}; const contracts = {}; const networks = []; const web3instances = {}
+const providers = {}; const contractWithProvider = {}; const networks = []; const web3instances = {}
 let activeNetwork = config.defaults.network
 let web3; let one
+let tokenContractTemplates = { erc20: IERC20, erc721: IERC721, erc1155: IERC1155 }
+let tokenMetadataTemplates = { erc20: IERC20Metadata, erc721: IERC721Metadata, erc1155: IERC1155MetadataURI }
+let tokenContractsWithProvider = { erc20: {}, erc721: {}, erc1155: {} }
+let tokenMetadataWithProvider = { erc20: {}, erc721: {}, erc1155: {} }
+let tokens = { erc20: null, erc721: null, erc1155: null }
+let tokenMetadata = { erc20: null, erc721: null, erc1155: null }
 
 const initBlockchain = (store) => {
   Object.keys(config.networks).forEach(k => {
@@ -64,21 +77,34 @@ const initBlockchain = (store) => {
       console.trace(ex)
     }
   })
+
   Object.keys(providers).forEach(k => {
     const c = contract(ONEWalletContract)
     c.setProvider(providers[k])
-    contracts[k] = c
+    contractWithProvider[k] = c
+    Object.keys(tokenContractsWithProvider).forEach(t => {
+      tokenContractsWithProvider[t][k] = contract(tokenContractTemplates[t])
+      tokenContractsWithProvider[t][k].setProvider(providers[k])
+      tokenMetadataWithProvider[t][k] = contract(tokenMetadataTemplates[t])
+      tokenMetadataWithProvider[t][k].setProvider(providers[k])
+    })
   })
-  web3 = web3instances[activeNetwork]
-  one = contracts[activeNetwork]
+  const switchNetwork = () => {
+    web3 = web3instances[activeNetwork]
+    one = contractWithProvider[activeNetwork]
+    Object.keys(tokens).forEach(t => {
+      tokens[t] = tokenContractsWithProvider[t][activeNetwork]
+      tokenMetadata[t] = tokenMetadataWithProvider[t][activeNetwork]
+    })
+  }
+  switchNetwork()
   store.subscribe(() => {
     const state = store.getState()
     const { network } = state.wallet
     if (network !== activeNetwork) {
       if (config.debug) console.log(`Switching blockchain provider: from ${activeNetwork} to ${network}`)
       activeNetwork = network
-      web3 = web3instances[activeNetwork]
-      one = contracts[activeNetwork]
+      switchNetwork()
     }
   })
   if (config.debug) console.log('blockchain init complete:', { networks })
@@ -145,20 +171,6 @@ const api = {
         minorVersion: minorVersion ? minorVersion.toNumber() : 0,
       }
     },
-    getTrackedTokens: async ({ address }) => {
-      const c = await one.at(address)
-      const result = c.getTrackedTokens()
-      const [tokenTypes, contracts, tokenIds] = Object.keys(result).map(k => result[k])
-      const tt = []
-      for (let i = 0; i < tokenTypes.length; i++) {
-        tt.push({
-          tokenType: tokenTypes[i],
-          contract: contracts[i],
-          tokenIds: tokenIds[i]
-        })
-      }
-      return tt
-    },
     getBalance: async ({ address }) => {
       const balance = await web3.eth.getBalance(address)
       return balance
@@ -172,6 +184,54 @@ const api = {
         commits.push({ hash: hashes[i], args: args[i], timestamp: timestamps[i], completed: completed[i] })
       }
       return commits
+    },
+    getTrackedTokens: async ({ address }) => {
+      const c = await one.at(address)
+      const result = c.getTrackedTokens()
+      const [tokenTypes, contracts, tokenIds] = Object.keys(result).map(k => result[k])
+      const tt = []
+      for (let i = 0; i < tokenTypes.length; i++) {
+        tt.push({
+          tokenType: tokenTypes[i],
+          contractAddress: contracts[i],
+          tokenId: tokenIds[i]
+        })
+      }
+      return tt
+    },
+    // returns Promise<BN>
+    tokenBalance: async ({ address, contractAddress, tokenType, tokenId }) => {
+      const ct = ONEConstants.TokenType[tokenType]
+      if (!ct) {
+        throw new Error(`Unknown token type: ${tokenType}`)
+      }
+      const c = await tokens[ct.toLowerCase()].at(contractAddress)
+      if (tokenType === ONEConstants.TokenType.ERC20) {
+        return c.balanceOf(address)
+      } else if (tokenType === ONEConstants.TokenType.ERC721) {
+        const owner = await c.ownerOf(tokenId)
+        // console.log(owner)
+        return owner === address ? new BN(1) : new BN(0)
+      } else if (tokenType === ONEConstants.TokenType.ERC1155) {
+        return c.balanceOf(address, tokenId)
+      }
+    },
+
+    getTokenMetadata: async ({ tokenType, contractAddress, tokenId }) => {
+      const ct = ONEConstants.TokenType[tokenType]
+      if (!ct) {
+        throw new Error(`Unknown token type: ${tokenType}`)
+      }
+      const c = await tokenMetadata[ct.toLowerCase()].at(contractAddress)
+      let name, symbol, uri
+      if (tokenType === ONEConstants.TokenType.ERC20) {
+        [name, symbol] = await Promise.all([c.name(), c.symbol()])
+      } else if (tokenType === ONEConstants.TokenType.ERC721) {
+        [name, symbol, uri] = await Promise.all([c.name(), c.symbol(), c.tokenURI(tokenId)])
+      } else if (tokenType === ONEConstants.TokenType.ERC1155) {
+        uri = await c.uri(tokenId)
+      }
+      return { name, symbol, uri }
     }
   },
   relayer: {
@@ -192,6 +252,15 @@ const api = {
       // })
       const { data } = await base.post('/reveal/transfer', { neighbors, index, eotp, dest, amount, address })
       return data
+    },
+
+    updateTrackToken: async ({ address, neighbors, index, eotp, tokenType, contractAddress, tokenId, track }) => {
+      return api.relayer.revealTokenOperation({ address, neighbors, index, eotp, tokenType, contractAddress, tokenId, operationType: track ? ONEConstants.OperationType.TRACK : ONEConstants.OperationType.UNTRACK, dest: ONEConstants.EmptyAddress, amount: '0', data: '0x' })
+    },
+
+    revealTokenOperation: async ({ address, neighbors, index, eotp, operationType, tokenType, contractAddress, tokenId, dest, amount, data }) => {
+      const { data: ret } = await base.post({ address, neighbors, index, eotp, operationType, tokenType, contractAddress, tokenId, dest, amount, data })
+      return ret
     },
     revealRecovery: async ({ neighbors, index, eotp, address }) => {
       const { data } = await base.post('/reveal/recovery', { neighbors, index, eotp, address })
