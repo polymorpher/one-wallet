b5e901aa422417f8efd6ceda3585b9d5eaac1087
Aaron Li <aaron@qokka.ai>
2021-07-18 04:18:22 -0700

    computeTokenOperationHash default data param

--- code/lib/onewallet.js
+++ code/lib/onewallet.js
@@ -193,7 +193,7 @@ const computeTokenKey = ({ tokenType, contractAddress, tokenId }) => {
 //   bytes32(bytes20(dest)),
 //   bytes32(amount)
 //   data
-const computeTokenOperationHash = ({ neighbor, index, eotp, operationType, tokenType, contractAddress, tokenId, dest, amount, data }) => {
+const computeTokenOperationHash = ({ neighbor, index, eotp, operationType, tokenType, contractAddress, tokenId, dest, amount, data = new Uint8Array() }) => {
   const indexBytes = new BN(index, 10).toArrayLike(Uint8Array, 'be', 4)
   const operationTypeBytes = new BN(operationType, 10).toArrayLike(Uint8Array, 'be', 32)
   const tokenTypeBytes = new BN(tokenType, 10).toArrayLike(Uint8Array, 'be', 32)
