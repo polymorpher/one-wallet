b9a58dfff36d9632a15d42a3172527e76ea8d959
Aaron Li <aaron@qokka.ai>
2021-07-19 01:25:41 -0700

    NFT Grid - fully functional

--- code/client/src/components/NFTGrid.jsx
+++ code/client/src/components/NFTGrid.jsx
@@ -1,23 +1,36 @@
-import { Card, Image, Row, Space, Typography, Col, Divider, Button, message } from 'antd'
+import { Card, Image, Row, Space, Typography, Col, Divider, Button, message, Carousel } from 'antd'
 import { unionWith, isNull, isUndefined, differenceBy } from 'lodash'
 import walletActions from '../state/modules/wallet/actions'
-
-import { PlusCircleOutlined } from '@ant-design/icons'
 import React, { useState, useEffect } from 'react'
-import { TallRow } from './Grid'
+import { AverageRow, TallRow } from './Grid'
 import { api } from '../../../lib/api'
 import ONE from '../../../lib/onewallet'
 import ONEUtil from '../../../lib/util'
-import util from '../util'
+import util, { useWindowDimensions } from '../util'
 import { Warning, Hint, InputBox, Heading } from './Text'
 import { withKeys, HarmonyONE } from './TokenAssets'
 import { useDispatch, useSelector } from 'react-redux'
-import abbr from '../abbr'
-import { handleAddressError } from '../handler'
 import ONEConstants from '../../../lib/constants'
-const { Text, Link } = Typography
+import { FallbackImage } from '../constants/ui'
+import styled from 'styled-components'
+import { LeftOutlined, RightOutlined } from '@ant-design/icons'
+import Paths from '../constants/paths'
+import { useHistory } from 'react-router'
+const { Text, Title } = Typography
 
-const NFTGridItem = ({ style, tokenType, name, symbol, uri, contractAddress, balance, selected, onSelected }) => {
+const GridItem = styled(Card.Grid)`
+  
+  &:hover{
+    opacity: ${props => props['data-full-view'] ? 1.0 : 0.5};
+  }
+`
+const SlickButtonFix = ({ currentSlide, slideCount, children, ...props }) => (
+  <span {...props}>{children}</span>
+)
+
+const NFTGridItem = ({ style, styleFullView, imageWrapperStyle, imageWrapperStyleFullView, tokenType, name, symbol, uri, contractAddress, balance, selected, onSend }) => {
+  const { isMobile } = useWindowDimensions()
+  const [fullView, setFullView] = useState(false)
   const bech32ContractAddress = util.safeOneAddress(contractAddress)
   const abbrBech32ContractAddress = util.ellipsisAddress(bech32ContractAddress)
   uri = util.replaceIPFSLink(uri)
@@ -34,23 +47,122 @@ const NFTGridItem = ({ style, tokenType, name, symbol, uri, contractAddress, bal
     }
     f()
   }, [])
-  const imageHeight = style.height - style.infoBarHeight
+  let displayName = metadata?.name
+  if (metadata?.properties?.collection) {
+    displayName = `${metadata?.name} | ${metadata.properties.collection}`
+  }
+  if (symbol) {
+    displayName = `${displayName} | ${symbol}`
+  }
+  let displayBalance = 'Not Owned'
+  if (balance) {
+    if (tokenType === ONEConstants.TokenType.ERC721) {
+      displayBalance = <Text style={{ color: 'purple' }}>Uniquely Owned</Text>
+    } else {
+      displayBalance = `Owned: ${balance}`
+    }
+  }
+  const animationUrl = metadata?.animation_url || metadata?.properties?.animation_url
+
+  const wrapperStyle = fullView ? imageWrapperStyleFullView : imageWrapperStyle
 
   return (
-    <Card.Grid style={{ ...style, ...(selected && { backgroundColor: '#fafafa' }) }} onClick={onSelected}>
-      <Row style={{ height: imageHeight }}>
-        <Image preview={false} src={util.replaceIPFSLink(metadata.image)} wrapperStyle={{ height: imageHeight, width: '100%' }} style={{ objectFit: 'cover' }} />
-      </Row>
-      <Row justify='space-between' style={{ alignItems: 'center' }} gutter={8}>
-        {metadata && <Text style={{ fontSize: 24 }}>{metadata.name}</Text>}
-        {!metadata && <Text copyable={{ text: abbrBech32ContractAddress }}>${util.ellipsisAddress(abbrBech32ContractAddress)}</Text>}
-        <Text style={{ fontSize: 24 }}>{tokenType === ONEConstants.TokenType.ERC721 ? (balance ? 'Owned' : 'Not Owned') : `Owned: ${balance}`}</Text>
-      </Row>
-    </Card.Grid>
+    <GridItem style={fullView ? styleFullView : style} hoverable={false} onClick={() => !fullView && setFullView(true)} data-full-view={fullView}>
+      {!fullView &&
+        <Row style={{ height: wrapperStyle.height || 'auto' }}>
+          <Image
+            preview={false} src={util.replaceIPFSLink(metadata?.image)} fallback={FallbackImage}
+            wrapperStyle={wrapperStyle} style={{ objectFit: 'cover', width: '100%', height: '100%' }}
+          />
+        </Row>}
+      {!fullView &&
+        <Row justify='space-between' style={{ padding: 8 }}>
+          {metadata && <Text style={{ fontSize: 12, lineHeight: '16px' }}>{displayName}</Text>}
+          {!metadata &&
+            <Text
+              style={{ fontSize: 12 }}
+              copyable={{ text: abbrBech32ContractAddress }}
+            >{util.ellipsisAddress(abbrBech32ContractAddress)}
+            </Text>}
+          <Text style={{ fontSize: 12, lineHeight: '16px' }}>{displayBalance}</Text>
+        </Row>}
+      {fullView &&
+        <Row style={{ height: wrapperStyle.height || 'auto' }}>
+          <Carousel
+            style={wrapperStyle} arrows autoplay autoplaySpeed={5000}
+            prevArrow={<SlickButtonFix><LeftOutlined /></SlickButtonFix>}
+            nextArrow={<SlickButtonFix><RightOutlined /></SlickButtonFix>}
+          >
+            <Image
+              onClick={() => setFullView(false)}
+              preview={false} src={util.replaceIPFSLink(metadata?.image)} fallback={FallbackImage}
+              wrapperStyle={wrapperStyle} style={{ objectFit: 'contain', width: '100%', height: '100%' }}
+            />
+            {animationUrl &&
+              <Image
+                onClick={() => setFullView(false)}
+                preview={false} src={util.replaceIPFSLink(animationUrl)} fallback={FallbackImage}
+                wrapperStyle={wrapperStyle} style={{ objectFit: 'contain', width: '100%', height: '100%' }}
+              />}
+          </Carousel>
+        </Row>}
+      {fullView && metadata &&
+        <div style={{ padding: 16 }}>
+          <Space direction='vertical' style={{ marginBottom: 16, width: '100%' }}>
+            <Row justify='space-between' style={{ width: '100%' }} align='middle'>
+              <Heading>{metadata.name}</Heading>
+              <Button type='primary' shape='round' size='large' onClick={() => onSend({ ...metadata, displayName })}>Send</Button>
+            </Row>
+
+            <Text>{metadata.description}</Text>
+            <AverageRow>
+              <Space size='large'>
+                {metadata.image && <Button type='link' href={util.replaceIPFSLink(metadata.image)} target='_blank' style={{ padding: 0 }}>Download Image</Button>}
+                {metadata.animation_url && <Button type='link' href={util.replaceIPFSLink(animationUrl)} target='_blank' style={{ padding: 0 }}>Download Animation</Button>}
+              </Space>
+            </AverageRow>
+          </Space>
+          {metadata?.properties?.collection &&
+            <AverageRow align='middle'>
+              <Col span={isMobile ? 24 : 12}> <Title level={3}>Collection</Title></Col>
+              <Col>
+                <Text>{metadata?.properties?.collection}</Text>
+              </Col>
+            </AverageRow>}
+          {name && symbol &&
+            <AverageRow align='middle'>
+              <Col span={isMobile ? 24 : 12}> <Title level={3}>Part Of</Title></Col>
+              <Col>
+                <Space>
+                  <Text>{name} ({symbol})</Text>
+                </Space>
+              </Col>
+            </AverageRow>}
+          <AverageRow align='middle'>
+            <Col span={isMobile ? 24 : 12}> <Title level={3}>Your Ownership</Title></Col>
+            <Col>
+              <Space>
+                <Text>{displayBalance}</Text>
+              </Space>
+            </Col>
+          </AverageRow>
+          {metadata?.properties?.creation_time &&
+            <AverageRow align='middle'>
+              <Col span={isMobile ? 24 : 12}> <Title level={3}>Created On</Title></Col>
+              <Col> <Text>{new Date(metadata?.properties?.creation_time).toLocaleString()}</Text> </Col>
+            </AverageRow>}
+          {metadata?.properties?.artist &&
+            <AverageRow align='middle'>
+              <Col span={isMobile ? 24 : 12}> <Title level={3}>Creator</Title></Col>
+              <Col> <Text>{metadata?.properties?.artist}</Text> </Col>
+            </AverageRow>}
+        </div>}
+    </GridItem>
   )
 }
 
 export const NFTGrid = ({ address }) => {
+  const history = useHistory()
   const dispatch = useDispatch()
   const wallet = useSelector(state => state.wallet.wallets[address])
   const selectedToken = util.isNFT(wallet.selectedToken) && wallet.selectedToken
@@ -59,7 +171,6 @@ export const NFTGrid = ({ address }) => {
   const walletOutdated = util.isWalletOutdated(wallet)
   const [currentTrackedTokens, setCurrentTrackedTokens] = useState(trackedTokens || [])
   const [disabled, setDisabled] = useState(true)
-  const [section, setSection] = useState()
   const gridItemStyle = {
     padding: 0,
     width: '296px',
@@ -70,6 +181,21 @@ export const NFTGrid = ({ address }) => {
     cursor: 'pointer',
     color: disabled && 'grey',
     opacity: disabled && 0.5
+  }
+  const gridItemStyleFullView = {
+    padding: 0,
+    width: '100%',
+    // minHeight: '800px',
+    display: 'flex',
+    flexDirection: 'column',
+  }
+  const imageWrapperStyle = {
+    height: '264px'
+  }
+  const imageWrapperStyleFullView = {
+    maxHeight: '600px',
+    width: '100%',
+    cursor: 'pointer',
   }
   useEffect(() => {
     if (walletOutdated) {
@@ -104,33 +230,42 @@ export const NFTGrid = ({ address }) => {
     dispatch(walletActions.trackTokens({ address, tokens: newTokens }))
   }, [currentTrackedTokens])
 
-  const onSelect = (key) => () => {
-    if (!key) return setSection(null)
+  const onSend = (key) => (metadata) => {
+    if (!key) return
     const token = currentTrackedTokens.find(t => t.key === key)
-    dispatch(walletActions.setSelectedToken({ token, address }))
+    dispatch(walletActions.setSelectedToken({ token: { metadata, ...token }, address }))
+    const oneAddress = util.safeOneAddress(address)
+    history.push(Paths.showAddress(oneAddress, 'transfer'))
   }
 
   return (
     <>
-      {disabled && <Warning style={{ marginTop: 32 }}>Your wallet is based on an outdated version. It cannot hold or send tokens. Please create a new wallet and migrate assets.</Warning>}
-      {!section &&
-        <TallRow>
-          {currentTrackedTokens.map(tt => {
-            const { icon, name, symbol, key, uri } = tt
-            const balance = tokenBalances[key]
-            return (
-              <NFTGridItem
-                disabled={disabled}
-                selected={selectedToken.key === key}
-                uri={uri}
-                key={key}
-                style={gridItemStyle}
-                icon={icon} name={name} symbol={symbol} balance={balance}
-                onSelected={onSelect(key)}
-              />
-            )
-          })}
-        </TallRow>}
+      {disabled && <Warning style={{ marginTop: 16, marginBottom: 16 }}>Your wallet is based on an outdated version. It cannot hold or send tokens or NFTs. Please create a new wallet and migrate assets.</Warning>}
+
+      <TallRow>
+        {currentTrackedTokens.map(tt => {
+          const { name, symbol, key, uri, contractAddress, tokenType } = tt
+          const balance = tokenBalances[key]
+          return (
+            <NFTGridItem
+              tokenType={tokenType}
+              imageWrapperStyle={imageWrapperStyle}
+              disabled={disabled}
+              selected={selectedToken.key === key}
+              uri={uri}
+              key={key}
+              style={gridItemStyle}
+              styleFullView={gridItemStyleFullView}
+              imageWrapperStyleFullView={imageWrapperStyleFullView}
+              name={name}
+              contractAddress={contractAddress}
+              symbol={symbol}
+              balance={balance}
+              onSend={onSend(key)}
+            />
+          )
+        })}
+      </TallRow>
     </>
   )
 }
