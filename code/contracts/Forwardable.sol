4c069f6e07298dfe4a520fdac5013d115731e5e9
Aaron Li <aaron@qokka.ai>
2021-08-17 13:56:01 -0700

    forwardable view mutability

--- code/contracts/Forwardable.sol
+++ code/contracts/Forwardable.sol
@@ -2,7 +2,7 @@
 pragma solidity ^0.8.4;
 
 abstract contract Forwardable{
-    function _getForwardAddress() internal virtual returns (address payable){
+    function _getForwardAddress() internal view virtual returns (address payable){
         return payable(address(0));
     }
 }
