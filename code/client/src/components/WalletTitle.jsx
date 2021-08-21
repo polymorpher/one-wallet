964d90f700d9246f3b465018edf174fb9b583a5f
Aaron Li <aaron@qokka.ai>
2021-08-20 23:59:34 -0700

    hide buy domain when address label is expanded

--- code/client/src/components/WalletTitle.jsx
+++ code/client/src/components/WalletTitle.jsx
@@ -1,7 +1,7 @@
 import { Button, Space, Typography } from 'antd'
 import WalletAddress from './WalletAddress'
 import util, { useWindowDimensions } from '../util'
-import React from 'react'
+import React, { useState } from 'react'
 import Paths from '../constants/paths'
 import { useHistory } from 'react-router'
 const { Title, Text } = Typography
@@ -9,6 +9,7 @@ const { Title, Text } = Typography
 const WalletTitle = ({ wallet }) => {
   const history = useHistory()
   const { isMobile } = useWindowDimensions()
+  const [showBuyDomain, setShowBuyDomain] = useState()
   const domainName = wallet.domain
   const hasDomainName = domainName && domainName !== ''
 
@@ -20,27 +21,22 @@ const WalletTitle = ({ wallet }) => {
   return (
     <Space size='large' align='baseline'>
       <Title level={2}>{wallet.name}</Title>
-      {
-        hasDomainName
-          ? <></>
-          : (
-            <Button type='primary' onClick={onPurchaseDomain}>
-              Buy Domain
-            </Button>
-            )
-      }
       <Space direction='vertical' size='small' align='start'>
-        {
-          hasDomainName ? <Text type='secondary'>{domainName}</Text> : <></>
-        }
+        {hasDomainName && <Text type='secondary'>{domainName}</Text>}
         <WalletAddress
           address={wallet.address}
+          onToggle={expanded => setShowBuyDomain(!expanded)}
           shorten={util.shouldShortenAddress({
             walletName: wallet.name,
             isMobile
           })}
         />
       </Space>
+      {!hasDomainName && showBuyDomain && (
+        <Button type='primary' shape='round' onClick={onPurchaseDomain}>
+          Get Domain Name
+        </Button>
+      )}
     </Space>
   )
 }
