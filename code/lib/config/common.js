19f03b461a53bcb72c4087c10bf4685890664122
Aaron Li <aaron@qokka.ai>
2021-09-09 20:02:20 -0700

    v0.10.3

--- code/lib/config/common.js
+++ code/lib/config/common.js
@@ -3,7 +3,7 @@ const DEBUG = process.env.DEBUG
 module.exports = {
   appId: 'ONEWallet',
   appName: '1wallet',
-  version: 'v0.10.2',
+  version: 'v0.10.3',
   minWalletVersion: parseInt(process.env.MIN_WALLET_VERSION || 3),
   minUpgradableVersion: parseInt(process.env.MIN_UPGRADABLE_WALLET_VERSION || 9),
   defaults: {
