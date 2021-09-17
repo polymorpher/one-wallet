e5b9f9617c2eb2fdab6bf86b5b3bfd58cfde40bd
Aaron Li <aaron@qokka.ai>
2021-09-17 03:22:45 -0700

    Gift: full flow

--- code/client/src/pages/Show/Gift.jsx
+++ code/client/src/pages/Show/Gift.jsx
@@ -13,7 +13,7 @@ import { Hint, InputBox, Label } from '../../components/Text'
 import AddressInput from '../../components/AddressInput'
 import { CommitRevealProgress } from '../../components/CommitRevealProgress'
 import AnimatedSection from '../../components/AnimatedSection'
-import util, { useWindowDimensions } from '../../util'
+import util, { generateOtpSeed, useWindowDimensions } from '../../util'
 import BN from 'bn.js'
 import ShowUtils from './show-util'
 import { SmartFlows } from '../../../../lib/api/flow'
@@ -24,8 +24,11 @@ import ONEConstants from '../../../../lib/constants'
 import { OtpStack, useOtpState } from '../../components/OtpStack'
 import { useRandomWorker } from './randomWorker'
 import humanizeDuration from 'humanize-duration'
-import { useMetadata, useNFTs } from '../../components/NFTGrid'
+import { useMetadata, useNFTs, useTokenBalanceTracker } from '../../components/NFTGrid'
 import { TallRow } from '../../components/Grid'
+import WalletConstants from '../../constants/wallet'
+import { handleAPIError } from '../../handler'
+import WalletCreateProgress from '../../components/WalletCreateProgress'
 const { Title, Text } = Typography
 
 const SimpleNFTRow = ({ nft, amount, onClick, onAmountChange, onDelete }) => {
@@ -42,7 +45,10 @@ const SimpleNFTRow = ({ nft, amount, onClick, onAmountChange, onDelete }) => {
     </Space>
   )
 }
-
+const sections = {
+  prepare: 0,
+  share: 1
+}
 const Gift = ({
   address,
   onSuccess, // optional
@@ -60,17 +66,90 @@ const Gift = ({
   const { state: otpState } = useOtpState()
   const { otpInput, otp2Input, resetOtp } = otpState
 
+  const [section, setSection] = useState(sections.prepare)
+
+  const tokenBalances = wallet.tokenBalances || {}
+
   const [totalAmountInput, setTotalAmountInput] = useState(prefilledTotalAmount || 100) // ONEs, string
   const [claimLimitInput, setClaimLimitInput] = useState(prefilledClaimLimit || 25) // ONEs, string
   const [claimInterval, setClaimInterval] = useState(prefilledClaimInterval || 30) // seconds, int
   const [selectedNFTs, setSelectedNFTs] = useState([])
   const { nfts, nftMap, loaded } = useNFTs({ address })
+  useTokenBalanceTracker({ tokens: nfts, address })
   const [nftAmounts, setNftAmounts] = useState([])
+
+  // generated stuff
+  const [confirmedMakingPacket, setConfirmedMakingPacket] = useState(false)
+  const [deploying, setDeploying] = useState(false)
+  const [seed] = useState(generateOtpSeed())
+  const [worker, setWorker] = useState()
+  const [slotSize] = useState(1)
+  const [progress, setProgress] = useState(0)
+  const [progressStage, setProgressStage] = useState(0)
+  const [redPacketAddress, setRedPacketAddress] = useState() // '0x12345678901234567890'
+  const [effectiveTime, setEffectiveTime] = useState()
+  const [qrCodeData, setQRCodeData] = useState()
 
   const { resetWorker, recoverRandomness } = useRandomWorker()
   const { prepareValidation, onRevealSuccess, ...handlers } = ShowUtils.buildHelpers({ setStage, resetOtp, network, resetWorker })
 
+  useEffect(() => {
+    const worker = new Worker('/ONEWalletWorker.js')
+    worker.onmessage = async (event) => {
+      const { status, current, total, stage, result } = event.data
+      if (status === 'working') {
+        // console.log(`Completed ${(current / total * 100).toFixed(2)}%`)
+        setProgress(Math.round(current / total * 100))
+        setProgressStage(stage)
+      }
+      if (status === 'done') {
+        const { root, layers, maxOperationsPerInterval } = result
+        // console.log('Received created wallet from worker:', result)
+        try {
+          const { address: newAddress } = await api.relayer.create({
+            root: ONEUtil.hexString(root),
+            height: layers.length,
+            interval: WalletConstants.interval / 1000,
+            t0: effectiveTime / WalletConstants.interval,
+            lifespan: WalletConstants.redPacketDuration / WalletConstants.interval,
+            slotSize: maxOperationsPerInterval,
+            lastResortAddress: address,
+            spendingLimit: ONEUtil.toFraction(claimLimitInput).toString(),
+            spendingInterval: claimInterval,
+          })
+          setRedPacketAddress(newAddress)
+        } catch (ex) {
+          handleAPIError(ex)
+          setDeploying(false)
+        }
+      }
+    }
+    setWorker(worker)
+  }, [])
+
+  useEffect(() => {
+    if (confirmedMakingPacket && worker) {
+      setDeploying(true)
+      // console.log('Posting to worker. Security parameters:', securityParameters)
+      const t = Math.floor(Date.now() / WalletConstants.interval) * WalletConstants.interval
+      setEffectiveTime(t)
+      worker && worker.postMessage({
+        seed,
+        effectiveTime: t,
+        duration: WalletConstants.redPacketDuration,
+        slotSize,
+        interval: WalletConstants.interval,
+        randomness: 0,
+        hasher: 'sha256'
+      })
+    }
+  }, [confirmedMakingPacket, worker])
+
   const createRedPacket = () => {
+    if (!redPacketAddress) {
+      message.error('INTERNAL ERROR: must make red packet 1wallet first')
+      return
+    }
     const { otp, otp2, invalidOtp2, invalidOtp, amount: totalAmount } = prepareValidation({ state: { otpInput, otp2Input, doubleOtp, transferAmount: totalAmountInput }, checkDest: false }) || {}
     if (invalidOtp || invalidOtp2) return
     const { spendingLimit, valid: spendingLimitValid } = util.toBalance(claimLimitInput)
@@ -87,17 +166,20 @@ const Gift = ({
         message.error(`Invalid amount for collectible at position ${i} `)
         return
       }
+      if (!(tokenBalances[selectedNFTs[i]] <= nftAmounts[i])) {
+        message.error(`Insufficient token balance at position ${i}`)
+        return
+      }
     }
-    const redpacketAddress = '' // TODO
     const calls = []
-    calls.push({ method: '', amount: totalAmount, dest: redpacketAddress })
+    calls.push({ method: '', amount: totalAmount, dest: redPacketAddress })
     for (let i = 0; i < selectedNFTs.length; i++) {
       const nft = nftMap[selectedNFTs[i]]
       const dest = nft.contractAddress
       if (nft.tokenType === ONEConstants.TokenType.ERC721) {
-        calls.push({ dest, method: 'safeTransferFrom(address,address,uint256,bytes)', values: [address, redpacketAddress, nft.tokenId, '0x'] })
+        calls.push({ dest, method: 'safeTransferFrom(address,address,uint256,bytes)', values: [address, redPacketAddress, nft.tokenId, '0x'] })
       } else if (nft.tokenType === ONEConstants.TokenType.ERC1155) {
-        calls.push({ dest, method: 'safeTransferFrom(address,address,uint256,uint256,bytes)', values: [address, redpacketAddress, nft.tokenId, nftAmounts[i], '0x'] })
+        calls.push({ dest, method: 'safeTransferFrom(address,address,uint256,uint256,bytes)', values: [address, redPacketAddress, nft.tokenId, nftAmounts[i], '0x'] })
       } else {
         message.error(`Invalid token type ${nft.tokenType} for token at position ${i}`)
         return
@@ -118,8 +200,16 @@ const Gift = ({
       afterCommit: () => setStage(2),
       revealAPI: api.relayer.reveal,
       revealArgs: { ...args, data: hexData },
-      ...handlers
+      ...handlers,
+      onRevealSuccess: (txId) => {
+        onRevealSuccess(txId)
+        setSection(sections.share)
+      }
     })
+  }
+
+  const confirmMakingRedPacket = () => {
+    setConfirmedMakingPacket(true)
   }
 
   return (
@@ -191,20 +281,32 @@ const Gift = ({
             </Select>
           </Space>
         </Space>
-        <TallRow>
-          <OtpStack walletName={wallet.name} doubleOtp={doubleOtp} otpState={otpState} />
-        </TallRow>
-        <Row justify='end' align='baseline'>
-          <Space>
-            {stage >= 0 && stage < 3 && <LoadingOutlined />}
-            {stage === 3 && <CheckCircleOutlined />}
-            <Button type='primary' size='large' shape='round' disabled={stage >= 0} onClick={createRedPacket}>Create Red Packet</Button>
-          </Space>
-        </Row>
+        {!redPacketAddress &&
+          <Row justify='end'>
+            <Button disabled={deploying} type='primary' shape='round' size='large' onClick={confirmMakingRedPacket}>Create Red Packet</Button>
+          </Row>}
+        {redPacketAddress &&
+          <>
+            <TallRow>
+              <OtpStack walletName={wallet.name} doubleOtp={doubleOtp} otpState={otpState} />
+            </TallRow>
+            <Row justify='end' align='baseline'>
+              <Space>
+                {stage >= 0 && stage < 3 && <LoadingOutlined />}
+                {stage === 3 && <CheckCircleOutlined />}
+                <Button
+                  type='primary' size='large' shape='round' disabled={stage >= 0}
+                  onClick={createRedPacket}
+                >Confirm Transfer Assets
+                </Button>
+              </Space>
+            </Row>
+          </>}
         <Hint>
           The collectibles you selected plus {totalAmountInput} ONE will be transferred to the red packet. The red packet is controlled by your wallet. You can reclaim the remaining funds and collectibles at any time. The red packet will automatically expire in a week.
         </Hint>
-        <CommitRevealProgress stage={stage} style={{ marginTop: 32 }} />
+        {deploying && <WalletCreateProgress progress={progress} isMobile={isMobile} progressStage={progressStage} />}
+        {redPacketAddress && <CommitRevealProgress stage={stage} style={{ marginTop: 32 }} />}
       </Space>
     </>
   )
