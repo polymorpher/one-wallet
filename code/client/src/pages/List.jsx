35bd98cc8af3b2b4cdb3b620b027d14a11c07058
Aaron Li <aaron@qokka.ai>
2021-09-19 00:31:34 -0700

    old way of split

--- code/client/src/pages/List.jsx
+++ code/client/src/pages/List.jsx
@@ -12,7 +12,7 @@ import storage from '../storage'
 const { Text, Title } = Typography
 
 const walletShortName = (fullName) => {
-  const walletNameParts = fullName.split(/[ -_]/)
+  const walletNameParts = fullName.split(' ')
 
   return walletNameParts.length > 1 ? `${walletNameParts[0]}...` : fullName
 }
