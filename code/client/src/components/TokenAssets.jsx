130efc46acaaeac6e6b26a80ec991dbedf445a64
Aaron Li <aaron@qokka.ai>
2021-10-13 01:10:52 -0700

    USDT decimals

--- code/client/src/components/TokenAssets.jsx
+++ code/client/src/components/TokenAssets.jsx
@@ -41,6 +41,7 @@ export const KnownERC20 = {
     icon: IconUSDT,
     symbol: '1USDT',
     name: 'Tether USD',
+    decimals: 6,
     contractAddress: '0x3C2B8Be99c50593081EAA2A724F0B8285F5aba8f'
   },
   bscBUSD: {
