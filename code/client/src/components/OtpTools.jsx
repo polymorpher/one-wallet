ec0d610ff1d70112f2d0d7dd520396579abdf2f2
Aaron Li <aaron@qokka.ai>
2021-10-30 06:27:15 -0700

    otp tools

--- code/client/src/components/OtpTools.jsx
+++ code/client/src/components/OtpTools.jsx
@@ -1,5 +1,9 @@
 import message from '../message'
 import { MigrationPayload } from '../proto/oauthMigration'
+import b32 from 'hi-base32'
+import { Image, Row } from 'antd'
+import { OSType } from '../util'
+import React from 'react'
 
 const OAUTH_OTP_PATTERN = /otpauth:\/\/totp\/(.+)(%3A|:)(.+)\?.+/
 export const parseOAuthOTP = (url) => {
@@ -47,3 +51,66 @@ export const parseMigrationPayload = url => {
   const name = filteredParams[0]?.name
   return { secret, secret2, name }
 }
+
+export const OTPUriMode = {
+  STANDARD: 0,
+  MIGRATION: 1,
+  TOTP: 2, // seems deprecated, should not use unless it is for testing
+}
+
+export const getQRCodeUri = (otpSeed, otpDisplayName, mode = OTPUriMode.STANDARD) => {
+  if (mode === OTPUriMode.STANDARD) {
+    // otpauth://TYPE/LABEL?PARAMETERS
+    return `otpauth://totp/${otpDisplayName}?secret=${b32.encode(otpSeed)}&issuer=Harmony`
+  }
+  if (mode === OTPUriMode.MIGRATION) {
+    const payload = MigrationPayload.create({
+      otpParameters: [{
+        issuer: 'Harmony',
+        secret: otpSeed,
+        name: otpDisplayName,
+        algorithm: MigrationPayload.Algorithm.ALGORITHM_SHA1,
+        digits: MigrationPayload.DigitCount.DIGIT_COUNT_SIX,
+        type: MigrationPayload.OtpType.OTP_TYPE_TOTP,
+      }],
+      version: 1,
+      batchIndex: 0,
+      batchSize: 1,
+    })
+    const bytes = MigrationPayload.encode(payload).finish()
+    const b64 = Buffer.from(bytes).toString('base64')
+    // console.log({ payload, bytes, b64 })
+    return `otpauth-migration://offline?data=${encodeURIComponent(b64)}`
+  }
+  return null
+}
+
+// not constructing qrCodeData on the fly (from seed) because generating a PNG takes noticeable amount of time. Caller needs to make sure qrCodeData is consistent with seed
+export const buildQRCodeComponent = ({ seed, name, os, isMobile, qrCodeData }) => {
+  const image = (url) =>
+    <Image
+      src={qrCodeData}
+      preview={false}
+      width={isMobile ? 192 : 256}
+      style={isMobile && { border: '1px solid lightgrey', borderRadius: 8, boxShadow: '0px 0px 10px lightgrey' }}
+      onClick={url && (() => window.open(url, '_self').focus())}
+    />
+  let href
+  if (os === OSType.iOS) {
+    href = getQRCodeUri(seed, name, OTPUriMode.MIGRATION)
+  } else if (os === OSType.Android) {
+    href = getQRCodeUri(seed, name, OTPUriMode.STANDARD)
+  } else if (isMobile) {
+    // To test in more devices
+    href = getQRCodeUri(seed, name, OTPUriMode.MIGRATION)
+  }
+
+  return (
+    <Row justify='center'>
+      {isMobile && qrCodeData && image(href)}
+      {!isMobile && qrCodeData && image()}
+    </Row>
+  )
+}
+
+export const getSecondCodeName = (name) => `${name} - 2nd`
