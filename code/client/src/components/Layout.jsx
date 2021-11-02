f7c48fd8dcecbb210ea1c5f95dbb3ef1f8ece916
Aaron Li <aaron@qokka.ai>
2021-11-02 06:15:50 -0700

    layout component

---
+++ code/client/src/components/Layout.jsx
@@ -0,0 +1,41 @@
+import { Button, Card, Popconfirm, Row, Space, Typography } from 'antd'
+import { AverageRow } from './Grid'
+import { deleteWalletLocally } from '../storage/util'
+import { DeleteOutlined } from '@ant-design/icons'
+import React from 'react'
+const { Text, Title, Link, Paragraph } = Typography
+
+const CardStyle = {
+  backgroundColor: 'rgba(0,0,0,0.15)',
+  position: 'absolute',
+  width: '100%',
+  height: '100%',
+  left: 0,
+  top: 0,
+  zIndex: 100,
+  backdropFilter: 'blur(10px)',
+  WebkitBackdropFilter: 'blur(10px)'
+}
+
+export const FloatContainer = ({ isMobile, children }) => {
+  return (
+    <Card style={CardStyle} bodyStyle={{ height: '100%' }}>
+      <Row justify='center'>
+        <Space
+          direction='vertical'
+          size='large'
+          style={{
+            height: '100%',
+            width: '100%',
+            maxWidth: 400,
+            justifyContent: 'start',
+            paddingTop: isMobile ? 32 : 192,
+            display: 'flex'
+          }}
+        >
+          {children}
+        </Space>
+      </Row>
+    </Card>
+  )
+}
