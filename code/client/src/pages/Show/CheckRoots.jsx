b4aa932184d8286c0cb6fe4d78c5ba245ce868b9
Aaron Li <aaron@qokka.ai>
2021-11-02 21:34:19 -0700

    check roots: more comprehensive expiry checking logics

--- code/client/src/pages/Show/CheckRoots.jsx
+++ code/client/src/pages/Show/CheckRoots.jsx
@@ -1,5 +1,6 @@
-import React, { useEffect, useState } from 'react'
+import React, { useCallback, useEffect, useState } from 'react'
 import util, { useWindowDimensions } from '../../util'
+import WalletConstants from '../../constants/wallet'
 import { Button, Typography, Space, Popconfirm } from 'antd'
 import storage from '../../storage'
 import { useHistory } from 'react-router'
@@ -10,15 +11,22 @@ import { useWallet } from '../../components/Common'
 import { AverageRow } from '../../components/Grid'
 import { FloatContainer } from '../../components/Layout'
 import { walletActions } from '../../state/modules/wallet'
-const { Title, Text } = Typography
+import humanizeDuration from 'humanize-duration'
+import WalletAddress from '../../components/WalletAddress'
+import { api } from '../../../../lib/api'
+import config from '../../config'
+import message from '../../message'
+import { doRetire } from './show-util'
+const { Title, Text, Link } = Typography
 
 const CheckRoots = ({ address, onClose }) => {
   const { isMobile } = useWindowDimensions()
   const history = useHistory()
   const { dispatch, wallets, wallet } = useWallet({ address })
-  const { oldInfos, root, lastResortAddress, acknowledgedRoot } = wallet
+  const { oldInfos, root, acknowledgedRoot, effectiveTime, duration, lastResortAddress, network } = wallet
   const [rootMissing, setRootMissing] = useState(false)
   const [oldRootExist, setOldRootExist] = useState(false)
+  const [oldRootExpiry, setOldRootExpiry] = useState(WalletConstants.defaultDuration)
   const [skip, setSkip] = useState(false)
   useEffect(() => {
     const f = async () => {
@@ -36,26 +44,69 @@ const CheckRoots = ({ address, onClose }) => {
             console.error(`No root exists in old core of address ${address}`, info)
             continue
           }
-          const oldLayers = await storage.getItem(root)
+          const oldLayers = await storage.getItem(info.root)
           if (oldLayers) {
             setOldRootExist(true)
+            const oldExpiry = info.effectiveTime + info.duration
+            const timeToExpiry = oldExpiry - Date.now()
+            setOldRootExpiry(timeToExpiry)
           }
         }
       }
     }
     f()
   }, [wallet?.root, wallet?.oldInfos])
-
+  const deleteAndRestore = async () => {
+    await deleteWalletLocally({ wallet, wallets, dispatch })
+    history.push(Paths.restore)
+  }
   const onAcknowledgeRoot = () => {
     dispatch(walletActions.userAcknowledgedNewRoot({ address, root }))
-    setSkip(true)
   }
-  if (skip || !rootMissing) {
+
+  if (skip) {
     return <></>
   }
 
+  if (!rootMissing) {
+    if (effectiveTime + duration > Date.now()) {
+      return (
+        <FloatContainer>
+          <Text>This wallet cannot be used because it is expired</Text>
+          <Text>You may transfer all its assets to its recovery address:</Text>
+          <WalletAddress showLabel address={lastResortAddress} alwaysShowOptions />
+          <Button type='primary' shape='round' onClick={() => doRetire({ address, network })}>Confirm</Button>
+          <Button shape='round' onClick={() => setSkip(true)}>Inspect Wallet</Button>
+          <Button shape='round' onClick={onClose}>Exit</Button>
+        </FloatContainer>
+      )
+    }
+  }
+
+  const DeleteAndRestore = useCallback(({ title, options }) => {
+    return (
+      <FloatContainer>
+        <Space direction='vertical'>
+          <Text>{title}</Text>
+          <AverageRow justify='center'>
+            {options}
+            <Popconfirm title='Are you sure？' onConfirm={() => deleteAndRestore()}>
+              <Button type='text' shape='round' danger size='large' icon={<DeleteOutlined />}>Delete And Restore</Button>
+            </Popconfirm>
+          </AverageRow>
+        </Space>
+      </FloatContainer>
+    )
+  }, [])
+
   if (oldRootExist) {
     if (acknowledgedRoot === root) {
+      if (oldRootExpiry < 0) {
+        return (<DeleteAndRestore title='This wallet cannot be used on this device. It is renewed elsewhere and its local data is expired. To use it on this device, please delete the wallet, then restore it' />)
+      }
+      if (oldRootExpiry < 3600 * 1000 * 24 * 30) {
+        return (<DeleteAndRestore title={`This wallet was renewed elsewhere. It is expiring in ${humanizeDuration(oldRootExpiry, { units: ['y', 'mo', 'd'], round: true })} on this device. To continue using it on this device, please delete the wallet, then restore it`} options={<Button type='primary' shape='round' onClick={() => setSkip(true)}>Remind me next time</Button>} />)
+      }
       return <></>
     }
     return (
@@ -73,27 +124,7 @@ const CheckRoots = ({ address, onClose }) => {
       </FloatContainer>
     )
   }
-  const deleteAndRestore = async () => {
-    await deleteWalletLocally({ wallet, wallets, dispatch })
-    history.push(Paths.restore)
-  }
-  const custom = (
-    <Space direction='vertical'>
-      <Text>This wallet cannot be used because its local storage is corrupted. Please delete the wallet, then restore it</Text>
-      <AverageRow justify='center'>
-        <Popconfirm title='Are you sure？' onConfirm={() => deleteAndRestore()}>
-          <Button type='text' shape='round' danger size='large' icon={<DeleteOutlined />}>Delete And Restore</Button>
-        </Popconfirm>
-      </AverageRow>
-    </Space>
-  )
-  // return (
-  //   <Warning custom={custom} />
-  // )
-  return (
-    <FloatContainer>
-      {custom}
-    </FloatContainer>
-  )
+
+  return (<DeleteAndRestore title='This wallet cannot be used because its local storage is corrupted. Please delete the wallet, then restore it' />)
 }
 export default CheckRoots
