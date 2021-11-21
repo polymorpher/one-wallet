b1b0c2eb9ac4e78928b50ece220a702fe66d02a9
Aaron Li <aaron@qokka.ai>
2021-11-21 05:38:04 -0800

    core test util: commitReveal, and  much better flow

--- code/test/util.js
+++ code/test/util.js
@@ -5,6 +5,9 @@ const config = require('../config')
 const base32 = require('hi-base32')
 const BN = require('bn.js')
 const unit = require('ethjs-unit')
+const ONE = require('../lib/onewallet')
+const ONEUtil = require('../lib/util')
+const ONEConstants = require('../lib/constants')
 const INTERVAL = 30000
 const Logger = {
   debug: (...args) => {
@@ -45,7 +48,7 @@ const makeCores = async ({
   hasher = OWUtil.sha256b }) => {
   let byteSeed = seed
   if (typeof seed === 'string') {
-    byteSeed = OWUtil.stringToBytes('0xdeadbeef1234567890123456789012')
+    byteSeed = OWUtil.stringToBytes(seed)
   }
   const otpSeed = OWUtil.base32Encode(byteSeed)
   const identificationKeys = [OWUtil.getIdentificationKey(byteSeed, true)]
@@ -53,7 +56,7 @@ const makeCores = async ({
   let byteSeed2 = seed2
   if (doubleOtp) {
     if (typeof seed2 === 'string') {
-      byteSeed2 = OWUtil.stringToBytes('0x1234567890deadbeef')
+      byteSeed2 = OWUtil.stringToBytes(seed2)
     }
     otpSeed2 = base32.encode(byteSeed2)
   }
@@ -66,9 +69,9 @@ const makeCores = async ({
     duration,
     randomness,
     hasher,
-    reportInterval: config.verbose ? 1 : null,
+    reportInterval: config.verbose ? (process.env.REPORT_INTERVAL || 100) : null,
     progressObserver: (i, n, s) => {
-      Logger.debug(`${((i + 1 / n) * 100).toFixed(2)}% (${i}/${n}) (Stage ${s})`)
+      Logger.debug(`${(((i + 1) / n) * 100).toFixed(2)}% (${i}/${n}) (Stage ${s})`)
     }
   })
   const height = layers.length
@@ -82,15 +85,15 @@ const makeCores = async ({
     const innerInterval = INTERVAL * 6
     const innerLifespan = Math.floor(duration / innerInterval)
     const innerT0 = Math.floor(effectiveTime / innerInterval)
-    innerCores.push([innerRoot, innerLayers.length, innerInterval / 1000, innerT0, innerLifespan, slotSize])
+    innerCores.push([ONEUtil.hexString(innerRoot), innerLayers.length, innerInterval / 1000, innerT0, innerLifespan, slotSize])
 
     if (!innerRoot) {
       throw new Error(`inner core has empty root: ${JSON.stringify(innerTree)}`)
     }
   }
-  Logger.debug('Inner Cores:', innerCores)
+  // Logger.debug('Inner Cores:', innerCores)
 
-  const core = [root, height, interval, t0, lifespan, slotSize]
+  const core = [ONEUtil.hexString(root), height, interval, t0, lifespan, slotSize]
 
   const vars = {
     otpSeed,
@@ -121,6 +124,7 @@ const makeCores = async ({
 }
 
 const createWallet = async ({
+  seed,
   effectiveTime,
   duration,
   lastResortAddress,
@@ -132,7 +136,7 @@ const createWallet = async ({
   spendingInterval = 86400,
   backlinks = []
 }) => {
-  const { core, innerCores, identificationKeys, vars } = makeCores({ maxOperationsPerInterval, doubleOtp, effectiveTime, duration, randomness, hasher })
+  const { core, innerCores, identificationKeys, vars } = await makeCores({ seed, maxOperationsPerInterval, doubleOtp, effectiveTime, duration, randomness, hasher })
   const initArgs = [
     core,
     [ new BN(spendingLimit), new BN(0), new BN(0), new BN(spendingInterval), new BN(0), new BN(spendingLimit) ],
@@ -142,17 +146,16 @@ const createWallet = async ({
     innerCores,
     identificationKeys,
   ]
-  Logger.debug('Creating ONEWallet contract with parameters', initArgs)
   const tx = await deploy(initArgs)
-  Logger.debug(tx)
+  Logger.debug('Creating ONEWallet contract with parameters', initArgs)
+  // Logger.debug(tx)
   const successLog = tx.logs.find(log => log.event === 'ONEWalletDeploySuccess')
   if (!successLog) {
     throw new Error('Wallet deploy unsuccessful')
   }
-  Logger.debug(successLog)
+  // Logger.debug(successLog)
   const address = successLog.args.addr
-  Logger.debug('Address', address)
-
+  // Logger.debug('Address', address)
   return {
     identificationKeys,
     address,
@@ -191,10 +194,55 @@ const increaseTime = async (seconds) => {
     id: 0,
   })
 }
+
+const commitReveal = async ({ layers, Debugger, index, eotp, paramsHash, commitParams, revealParams, wallet }) => {
+  const neighbors = ONE.selectMerkleNeighbors({ layers, index })
+  const neighbor = neighbors[0]
+  const { hash: commitHash } = ONE.computeCommitHash({ neighbor, index, eotp })
+  if (typeof paramsHash === 'function') {
+    const { hash } = paramsHash({ ...commitParams })
+    paramsHash = hash
+  }
+  const { hash: verificationHash } = ONE.computeVerificationHash({ paramsHash, eotp })
+  Logger.debug(`Committing`, { commitHash: ONEUtil.hexString(commitHash), paramsHash: ONEUtil.hexString(paramsHash), verificationHash: ONEUtil.hexString(verificationHash) })
+  await wallet.commit(ONEUtil.hexString(commitHash), ONEUtil.hexString(paramsHash), ONEUtil.hexString(verificationHash))
+  Logger.debug(`Committed`)
+  const neighborsEncoded = neighbors.map(ONEUtil.hexString)
+  Debugger.debugProof({ neighbors, height: layers.length, index, eotp, root: layers[layers.length - 1] })
+  const commits = await wallet.lookupCommit(ONEUtil.hexString(commitHash))
+  // console.log(commits)
+  const commitHashCommitted = commits[0][0]
+  const paramHashCommitted = commits[1][0]
+  const verificationHashCommitted = commits[2][0]
+  const timestamp = commits[3][0]
+  const completed = commits[4][0]
+  Logger.debug({ commit: { commitHashCommitted, paramHashCommitted, verificationHashCommitted, timestamp, completed }, currentTimeInSeconds: Math.floor(Date.now() / 1000) })
+  const authParams = [neighborsEncoded, index, ONEUtil.hexString(eotp)]
+  if (!revealParams.length) {
+    const { operationType, tokenType, contractAddress, tokenId, dest, amount, data } = { ...ONEConstants.NullOperationParams, ...revealParams }
+    revealParams = [operationType, tokenType, contractAddress, tokenId, dest, amount, data]
+  }
+  Logger.debug(`Revealing`, { authParams, revealParams })
+  const wouldSucceed = await wallet.reveal.call(authParams, revealParams)
+  Logger.debug(`Reveal success prediction`, !!wouldSucceed)
+  const tx = await wallet.reveal(authParams, revealParams)
+  return { tx, authParams, revealParams }
+}
+
+const printInnerTrees = ({ Debugger, innerTrees }) => {
+  for (let [index, innerTree] of innerTrees.entries()) {
+    const { layers: innerLayers, root: innerRoot } = innerTree
+    console.log(`Inner tree ${index}, root=${ONEUtil.hexString(innerRoot)}`)
+    Debugger.printLayers({ layers: innerLayers })
+  }
+}
+
 module.exports = {
   increaseTime,
   createWallet,
   makeCores,
   Logger,
-  getFactory: (factory) => Factories[factory]
+  getFactory: (factory) => Factories[factory],
+  commitReveal,
+  printInnerTrees
 }
