4fc3487e5645ef3e4b2fc00e802bb2e02d72bd02
Aaron Li <aaron@qokka.ai>
2021-12-11 07:38:13 -0800

    simplify GA messages

--- code/client/src/components/ScanGASteps.jsx
+++ code/client/src/components/ScanGASteps.jsx
@@ -4,9 +4,9 @@ const { Step } = Steps
 
 const ScanGASteps = () => (
   <Steps current={0} direction='vertical'>
-    <Step title='Open Google Authenticator' description='Go to Google Authenticator, tap ... -> Export accounts on the top right corner' />
-    <Step title='Select Your Wallet' description='Make sure your wallet is selected. Unselect other accounts.' />
-    <Step title='Scan the QR code' description='Scan the exported QR code on your Google Authenticator app' />
+    <Step title='Open Google Authenticator' description='Tap ... (top right corner) -> Export accounts' />
+    <Step title='Select Your Wallet' description='Unselect all accounts. Select your wallet only' />
+    <Step title='Scan the QR code' description='Point the exported QR code to your webcam' />
   </Steps>
 )
 
