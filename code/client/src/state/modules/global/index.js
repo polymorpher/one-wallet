e49407d7eef32798901186d2d6554f848205575e
Tao Zhou <iamzhoutao92@gmail.com>
2021-12-12 15:29:37 +0100

    Persist the global state

--- code/client/src/state/modules/global/index.js
+++ code/client/src/state/modules/global/index.js
@@ -3,4 +3,8 @@ import reducer from './reducers'
 export { default as globalSagas } from './sagas'
 export { default as globalActions } from './actions'
 
+export const persistConfig = {
+  key: 'global',
+}
+
 export default reducer
--- code/client/src/state/modules/wallet/index.js
+++ code/client/src/state/modules/wallet/index.js
@@ -3,4 +3,9 @@ import reducer from './reducers'
 export { default as walletSagas } from './sagas'
 export { default as walletActions } from './actions'
 
+export const persistConfig = {
+  key: 'wallet',
+  blacklist: ['layers', 'provider', 'error', 'fetching', 'loading']
+}
+
 export default reducer
