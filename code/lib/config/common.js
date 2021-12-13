b099b98f80d1a40a283b47bb70b227fac9ac47d9
Aaron Li <aaron@qokka.ai>
2021-12-13 01:50:33 -0800

    add default addresses for deployment

--- code/lib/config/common.js
+++ code/lib/config/common.js
@@ -21,9 +21,9 @@ module.exports = {
         url: 'http://127.0.0.1:7545',
         // explorer: 'https://explorer.harmony.one/#/tx/{{txId}}',
         deploy: {
-          factory: process.env.DEPLOY_FACTORY_GANACHE,
-          deployer: process.env.DEPLOY_DEPLOYER_GANACHE,
-          codeHelper: process.env.DEPLOY_CODE_HELPER_GANACHE
+          factory: process.env.DEPLOY_FACTORY_GANACHE || '0x69b19f89102DBdb3E1E8709d2CB9cB6b26a0Cc59',
+          deployer: process.env.DEPLOY_DEPLOYER_GANACHE || '0x58bbE690cA4ec94D034d91D05d6949661F2bE8E2',
+          codeHelper: process.env.DEPLOY_CODE_HELPER_GANACHE || '0x1d560f5904De502f34b556eec01bEc4a97665028'
         },
       }
     }),
@@ -34,9 +34,9 @@ module.exports = {
       production: true,
       chainId: 1,
       deploy: {
-        factory: process.env.DEPLOY_FACTORY_MAINNET,
-        deployer: process.env.DEPLOY_DEPLOYER_MAINNET,
-        codeHelper: process.env.DEPLOY_CODE_HELPER_MAINNET
+        factory: process.env.DEPLOY_FACTORY_MAINNET || '0x743AE189917d5b762CEDbC51FAC4e0C5Adf43132',
+        deployer: process.env.DEPLOY_DEPLOYER_MAINNET || '0x0c748D3De066f3c51D1F147b0C74b8E3D999d9c8',
+        codeHelper: process.env.DEPLOY_CODE_HELPER_MAINNET || '0xa17A4e2dbC0B6Ce3EE961553967ffE11224113E5',
       },
     },
     'harmony-testnet': {
@@ -44,9 +44,9 @@ module.exports = {
       url: 'https://api.s0.b.hmny.io',
       chainId: 2,
       deploy: {
-        factory: process.env.DEPLOY_FACTORY_TESTNET,
-        deployer: process.env.DEPLOY_DEPLOYER_TESTNET,
-        codeHelper: process.env.DEPLOY_CODE_HELPER_TESTNET
+        factory: process.env.DEPLOY_FACTORY_TESTNET || '0x12CfEeB5D792Fb5C5b06b166b14706AE4A14bC6d',
+        deployer: process.env.DEPLOY_DEPLOYER_TESTNET || '0xac1adA40E096Ee31D3EB0E14830ECAC1F0D18b38',
+        codeHelper: process.env.DEPLOY_CODE_HELPER_TESTNET || '0x1d560f5904De502f34b556eec01bEc4a97665028',
       },
     }
   },
