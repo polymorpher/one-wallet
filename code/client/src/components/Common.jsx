12ae1eb814e7721d5dc232c1be519f54ade544ae
Aaron Li <aaron@qokka.ai>
2022-01-16 00:49:28 -0800

    useSuperOps, useOpsBase, etc.

--- code/client/src/components/Common.jsx
+++ code/client/src/components/Common.jsx
@@ -14,12 +14,27 @@ export const useWallet = ({ address }) => {
   }
 }
 
-export const useOps = ({ address }) => {
+const useOpsBase = ({ address }) => {
   const { dispatch, wallets, wallet, network } = useWallet({ address })
   const [stage, setStage] = useState(-1)
+  const { isMobile, os } = useWindowDimensions()
+  return {
+    dispatch,
+    wallets,
+    wallet,
+    network,
+    stage,
+    setStage,
+    isMobile,
+    os
+  }
+}
+
+export const useOps = ({ address }) => {
+  const { dispatch, wallets, wallet, network, stage, setStage, isMobile, os } = useOpsBase({ address })
   const { resetWorker, recoverRandomness } = useRandomWorker()
   const { state: otpState } = useOtpState()
-  const { isMobile, os } = useWindowDimensions()
+
   return {
     dispatch,
     wallets,
@@ -30,6 +45,58 @@ export const useOps = ({ address }) => {
     resetWorker,
     recoverRandomness,
     otpState: { doubleOtp: wallet.doubleOtp, ...otpState },
+    isMobile,
+    os
+  }
+}
+
+export const useSuperOps = ({ address }) => {
+  const { dispatch, wallets, wallet, network, stage, setStage, isMobile, os } = useOpsBase({ address })
+  const otpStates = new Array(6).fill(0).map(() => useOtpState().state)
+  const resetOtps = () => {
+    for (let i = otpStates.length - 1; i >= 0; i--) {
+      otpStates[i].resetOtp(i > 0)
+    }
+    setStage(-1)
+  }
+
+  return {
+    dispatch,
+    wallets,
+    wallet,
+    network,
+    resetOtps,
+    stage,
+    setStage,
+    otpStates,
+    isMobile,
+    os
+  }
+}
+
+export const useOpsCombo = ({ address }) => {
+  const { dispatch, wallets, wallet, network, stage, setStage, isMobile, os } = useOpsBase({ address })
+  const otpStates = new Array(6).fill(0).map(() => useOtpState().state)
+  const { resetWorker, recoverRandomness } = useRandomWorker()
+  const { state: otpState } = useOtpState()
+  const resetOtps = () => {
+    for (let i = otpStates.length - 1; i >= 0; i--) {
+      otpStates[i].resetOtp(i > 0)
+    }
+    setStage(-1)
+  }
+  return {
+    dispatch,
+    wallets,
+    wallet,
+    network,
+    stage,
+    setStage,
+    resetWorker,
+    recoverRandomness,
+    resetOtps,
+    otpState: { doubleOtp: wallet.doubleOtp, ...otpState },
+    otpStates,
     isMobile,
     os
   }
