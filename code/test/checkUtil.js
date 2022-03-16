d39be144a90749a6ab00cd91c7f1a0642cfff9d4
John Whitton <john@johnwhitton.com>
2022-03-16 14:28:25 -0700

    Move utility functions into checkUtil

--- code/test/checkUtil.js
+++ code/test/checkUtil.js
@@ -1,3 +1,145 @@
+const TestUtil = require('./util')
+const ONEUtil = require('../lib/util')
+const unit = require('ethjs-unit')
+const ONE = require('../lib/onewallet')
+const BN = require('bn.js')
+const ONEConstants = require('../lib/constants')
+const ONEWallet = require('../lib/onewallet')
+const ONEDebugger = require('../lib/debug')
+const Logger = TestUtil.Logger
+const Debugger = ONEDebugger(Logger)
+const TestERC20 = artifacts.require('TestERC20')
+const TestERC20Decimals9 = artifacts.require('TestERC20Decimals9')
+const TestERC721 = artifacts.require('TestERC721')
+const TestERC1155 = artifacts.require('TestERC1155')
+
+const ONE_CENT = unit.toWei('0.01', 'ether')
+const HALF_DIME = unit.toWei('0.05', 'ether')
+const ONE_DIME = unit.toWei('0.1', 'ether')
+const ONE_ETH = unit.toWei('1', 'ether')
+const TEN_ETH = unit.toWei('10', 'ether')
+const INTERVAL = 30000
+const DURATION = INTERVAL * 12
+const SLOT_SIZE = 1
+const EFFECTIVE_TIME = Math.floor(Date.now() / INTERVAL / 6) * INTERVAL * 6 - DURATION / 2
+
+const sleep = (milliseconds) => {
+  return new Promise(resolve => setTimeout(resolve, milliseconds))
+}
+const getReceipt = async (tx) => {
+  let transactionReceipt = null
+  let i = 0
+  while (transactionReceipt == null && i < 10) { // Waiting expectedBlockTime until the transaction is mined
+    transactionReceipt = await web3.eth.getTransactionReceipt(tx.transactionHash)
+    if (transactionReceipt !== null) { break }
+    await sleep(1000)
+    i++
+    console.log(`waiting`)
+  }
+  assert.notEqual(null, transactionReceipt, `transactionReceipt not found for ${tx.transactionHash} after waiting 10 seconds`)
+  // if (transactionReceipt == null) {console.log(`Could not find transaction receipt for `)} 
+}
+
+// makeWallet uses an index and unlocked web3.eth.account and creates and funds a ONEwallet
+const makeWallet = async (accountIndex, lastResortAddress) => {
+  const { wallet, seed, hseed, root, client: { layers } } = await TestUtil.createWallet({
+    salt: new BN(accountIndex),
+    effectiveTime: EFFECTIVE_TIME,
+    duration: DURATION,
+    maxOperationsPerInterval: SLOT_SIZE,
+    lastResortAddress: lastResortAddress,
+    spendingLimit: ONE_ETH
+  })
+  // Fund wallet
+  // console.log(`ONE_CENT : ${ONE_CENT}`)
+  // console.log(`HALF_DIME: ${HALF_DIME}`)
+  // console.log(`ONE_DIME : ${ONE_DIME}`)
+  // console.log(`ONE_ETH  : ${ONE_ETH}`)
+  // console.log(`TEN_ETH  : ${TEN_ETH}`)
+  let tx = await web3.eth.sendTransaction({
+    from: lastResortAddress,
+    to: wallet.address,
+    value: TEN_ETH
+  })
+  getReceipt(tx)
+  // const InitialWalletBalance = await web3.eth.getBalance(wallet.address)
+  // console.log(`InitialWalletBalance: ${InitialWalletBalance}`)
+  // console.log(`lastResortAddressBalance: ${lastResortAddressBalance}`)
+  // console.log(`ONE_ETH: ${ONE_ETH}`)
+  const state = await getONEWalletState(wallet)
+  return { wallet, seed, hseed, root, layers, lastResortAddress, state }
+}
+
+// makeTokens makes test ERC20, ERC20Decimals9, ERC721, ERC1155
+const makeTokens = async (owner) => {
+  // create an ERC20
+  const testerc20 = await TestERC20.new(10000000, { from: owner })
+  // create an ERC20Decimals9
+  const testerc20d9 = await TestERC20Decimals9.new(10000000, { from: owner })
+  // create an ERC721
+  const tids = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+  const uris = ['ipfs://test721/0', 'ipfs://test721/1', 'ipfs://test721/2', 'ipfs://test721/3', 'ipfs://test721/4', 'ipfs://test721/5', 'ipfs://test721/6', 'ipfs://test721/7', 'ipfs://test721/8', 'ipfs://test721/9']
+  const testerc721 = await TestERC721.new(tids, uris, { from: owner })
+  // create an ERC1155
+  const tids1155 = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+  const amounts1155 = [10, 20, 20, 20, 20, 20, 20, 20, 20, 100]
+  const uris1155 = ['ipfs://test1155/0', 'ipfs://test1155/1', 'ipfs://test1155/2', 'ipfs://test1155/3', 'ipfs://test1155/4', 'ipfs://test1155/5', 'ipfs://test1155/6', 'ipfs://test1155/7', 'ipfs://test1155/8', 'ipfs://test1155/9']
+  const testerc1155 = await TestERC1155.new(tids1155, amounts1155, uris1155, { from: owner })
+  return { testerc20, testerc20d9, testerc721, testerc1155 }
+}
+
+// assetTransfer commits and reveals a wallet transaction
+const assetTransfer = async ({ wallet, operationType, tokenType, contractAddress, tokenId, dest, amount }) => {
+  Debugger.printLayers({ layers: wallet.layers })
+  const otp = ONEUtil.genOTP({ seed: wallet.seed })
+  const index = ONEUtil.timeToIndex({ effectiveTime: EFFECTIVE_TIME })
+  const eotp = await ONE.computeEOTP({ otp, hseed: wallet.hseed })
+  // Format commit and revealParams based on tokenType
+  let commitParams
+  let revealParams
+  let paramsHash
+  switch (operationType) {
+    case ONEConstants.OperationType.TRANSFER:
+      paramsHash = ONEWallet.computeTransferHash
+      commitParams = { dest, amount }
+      revealParams = { dest, amount, operationType }
+      break
+    case ONEConstants.OperationType.TRANSFER_TOKEN:
+      paramsHash = ONEWallet.computeGeneralOperationHash
+      switch (tokenType) {
+        case ONEConstants.TokenType.ERC20:
+          commitParams = { operationType, tokenType, contractAddress, dest, amount }
+          revealParams = { operationType, tokenType, contractAddress, dest, amount }
+          break
+        case ONEConstants.TokenType.ERC721:
+          commitParams = { operationType, tokenType, contractAddress, tokenId, dest }
+          revealParams = { operationType, tokenType, contractAddress, tokenId, dest }
+          break
+        case ONEConstants.TokenType.ERC1155:
+          commitParams = { operationType, tokenType, contractAddress, tokenId, dest, amount }
+          revealParams = { operationType, tokenType, contractAddress, tokenId, dest, amount }
+          break
+        default:
+          console.log(`TODO: add in Token error handling`)
+          return
+      }
+      break
+    default:
+      console.log(`TODO: add in error handling`)
+      return
+  }
+  await TestUtil.commitReveal({
+    Debugger,
+    layers: wallet.layers,
+    index,
+    eotp,
+    paramsHash,
+    commitParams,
+    revealParams,
+    wallet: wallet.wallet
+  })
+}
+
 // get OneWallet state
 const getONEWalletState = async (wallet) => {
   let state = {}
@@ -40,6 +182,9 @@ const checkONEWallet = async (wallet, state) => {
 }
 
 module.exports = {
+  makeWallet,
+  makeTokens,
+  assetTransfer,
   getONEWalletState,
   checkONEWallet
 }
--- code/test/tokens.js
+++ code/test/tokens.js
@@ -18,7 +18,7 @@ const EFFECTIVE_TIME = Math.floor(Date.now() / INTERVAL / 6) * INTERVAL * 6 - DU
 const ONE_CENT = unit.toWei('0.01', 'ether')
 // const HALF_DIME = unit.toWei('0.05', 'ether')
 // const ONE_DIME = unit.toWei('0.1', 'ether')
-// const ONE_ETH = unit.toWei('1', 'ether')
+const ONE_ETH = unit.toWei('1', 'ether')
 const TEN_ETH = unit.toWei('10', 'ether')
 
 const Logger = TestUtil.Logger
@@ -38,13 +38,13 @@ contract('ONEWallet', (accounts) => {
   // Transfer Native Token to external wallet
   it('Wallet_CommitReveal: Native Token Transfer must commit and reveal successfully', async () => {
     // Create Wallets and tokens
-    const alice = await makeWallet(1, accounts[1])
-    const bob = await makeWallet(2, accounts[2])
+    const alice = await CheckUtil.makeWallet(1, accounts[1])
+    const bob = await CheckUtil.makeWallet(2, accounts[2])
     const aliceInitialWalletBalance = await web3.eth.getBalance(alice.wallet.address)
     const bobInitialWalletBalance = await web3.eth.getBalance(bob.wallet.address)
     assert.equal(TEN_ETH, aliceInitialWalletBalance, 'Alice Wallet initially has correct balance')
     // alice tranfers ONE CENT to bob
-    await tokenTransfer(
+    await CheckUtil.assetTransfer(
       {
         wallet: alice,
         operationType: ONEConstants.OperationType.TRANSFER,
@@ -78,9 +78,9 @@ contract('ONEWallet', (accounts) => {
   // ERC20 Token Testing (Transfer, Mint, Track, SpendingLimit)
   it('Wallet_CommitReveal: ERC20(Transfer, Mint, Track) must commit and reveal successfully', async () => {
     // Create Wallets and tokens
-    const alice = await makeWallet(1, accounts[1])
-    const bob = await makeWallet(2, accounts[2])
-    const { testerc20 } = await makeTokens(alice.lastResortAddress)
+    const alice = await CheckUtil.makeWallet(1, accounts[1])
+    const bob = await CheckUtil.makeWallet(2, accounts[2])
+    const { testerc20 } = await CheckUtil.makeTokens(alice.lastResortAddress)
     let aliceBalanceERC20
     let aliceWalletBalanceERC20
     let bobBalanceERC20
@@ -90,7 +90,7 @@ contract('ONEWallet', (accounts) => {
     aliceWalletBalanceERC20 = await testerc20.balanceOf(alice.wallet.address)
     assert.equal(1000, aliceWalletBalanceERC20, 'Transfer of 1000 ERC20 tokens to alice.wallet succesful')
     // alice transfers tokens to bob
-    await tokenTransfer(
+    await CheckUtil.assetTransfer(
       {
         wallet: alice,
         operationType: ONEConstants.OperationType.TRANSFER_TOKEN,
@@ -131,11 +131,11 @@ contract('ONEWallet', (accounts) => {
   // ERC20 Decimals 9 Testing (Transfer, Mint, Track, SpendingLimit)
   it('Wallet_CommitReveal: ERC20-9(Transfer, Mint, Track) must commit and reveal successfully', async () => {
     // Create Wallets and tokens
-    const alice = await makeWallet(1, accounts[1])
+    const alice = await CheckUtil.makeWallet(1, accounts[1])
     const aliceInitialWalletBalance = await web3.eth.getBalance(alice.wallet.address)
     assert.equal(TEN_ETH, aliceInitialWalletBalance, 'Alice Wallet initially has correct balance')
-    const bob = await makeWallet(2, accounts[2])
-    const { testerc20d9 } = await makeTokens(alice.lastResortAddress)
+    const bob = await CheckUtil.makeWallet(2, accounts[2])
+    const { testerc20d9 } = await CheckUtil.makeTokens(alice.lastResortAddress)
     let aliceWalletBalanceERC20d9
     let bobWalletBalanceERC20d9
     // transfer ERC20d9 tokens from alice.lastResortAddress (which owns the tokens) to alices wallet
@@ -143,7 +143,7 @@ contract('ONEWallet', (accounts) => {
     aliceWalletBalanceERC20d9 = await testerc20d9.balanceOf(alice.wallet.address)
     assert.equal(1000, aliceWalletBalanceERC20d9, 'Transfer of 1000 ERC20d9 tokens to alice.wallet succesful')
     // alice transfers tokens to bob
-    await tokenTransfer(
+    await CheckUtil.assetTransfer(
       {
         wallet: alice,
         operationType: ONEConstants.OperationType.TRANSFER_TOKEN,
@@ -183,11 +183,11 @@ contract('ONEWallet', (accounts) => {
   // ERC721 Testing (Transfer, Mint, Track)
   it('Wallet_CommitReveal: ERC721(Transfer, Mint, Track) must commit and reveal successfully', async () => {
     // Create Wallets and tokens
-    const alice = await makeWallet(1, accounts[1])
+    const alice = await CheckUtil.makeWallet(1, accounts[1])
     const aliceInitialWalletBalance = await web3.eth.getBalance(alice.wallet.address)
     assert.equal(TEN_ETH, aliceInitialWalletBalance, 'Alice Wallet initially has correct balance')
-    const bob = await makeWallet(2, accounts[2])
-    const { testerc721 } = await makeTokens(alice.lastResortAddress)
+    const bob = await CheckUtil.makeWallet(2, accounts[2])
+    const { testerc721 } = await CheckUtil.makeTokens(alice.lastResortAddress)
     let aliceWalletBalanceERC721
     let bobWalletBalanceERC721
     assert.equal(alice.lastResortAddress, await testerc721.ownerOf(8), 'Alice.lastResortAddress owns token 8')
@@ -201,7 +201,7 @@ contract('ONEWallet', (accounts) => {
     assert.equal(alice.wallet.address, await testerc721.ownerOf(9), 'Transfer of ERC721 token 9 to alice.wallet succesful')
     console.log(`alice.wallet.getTrackedTokens(): ${JSON.stringify(await alice.wallet.getTrackedTokens())}`)
     // alice transfers tokens to bob
-    await tokenTransfer(
+    await CheckUtil.assetTransfer(
       {
         wallet: alice,
         operationType: ONEConstants.OperationType.TRANSFER_TOKEN,
@@ -244,11 +244,11 @@ contract('ONEWallet', (accounts) => {
 
   // ERC1155 Testing (Transfer, Mint, Track) 
   it('Wallet_CommitReveal: ERC1155(Transfer, Mint, Track) must commit and reveal successfully', async () => {
-    const alice = await makeWallet(1, accounts[1])
+    const alice = await CheckUtil.makeWallet(1, accounts[1])
     const aliceInitialWalletBalance = await web3.eth.getBalance(alice.wallet.address)
     assert.equal(TEN_ETH, aliceInitialWalletBalance, 'Alice Wallet initially has correct balance')
-    const bob = await makeWallet(2, accounts[2])
-    const { testerc1155 } = await makeTokens(alice.lastResortAddress)
+    const bob = await CheckUtil.makeWallet(2, accounts[2])
+    const { testerc1155 } = await CheckUtil.makeTokens(alice.lastResortAddress)
     let aliceWalletBalanceERC1155T8
     let bobWalletBalanceERC1155T8
     assert.equal(20, await testerc1155.balanceOf(alice.lastResortAddress, 8), 'Alice.lastResortAddress owns 20 of token 8')
@@ -258,7 +258,7 @@ contract('ONEWallet', (accounts) => {
     aliceWalletBalanceERC1155T8 = await testerc1155.balanceOf(alice.wallet.address, 8)
     assert.equal(8, aliceWalletBalanceERC1155T8, 'Transfer of 8 ERC721 token to alice.wallet succesful')
     // alice transfers tokens to bob
-    await tokenTransfer(
+    await CheckUtil.assetTransfer(
       {
         wallet: alice,
         operationType: ONEConstants.OperationType.TRANSFER_TOKEN,
@@ -300,14 +300,14 @@ contract('ONEWallet', (accounts) => {
 
   // TokenTracker Testing (track, multitrack, getTrackedTokens, getBalance, recoverToken) also batch transactions
   it('Wallet_CommitReveal: TokenTracker(token management) must commit and reveal successfully', async () => {
-    const alice = await makeWallet(1, accounts[1])
-    const bob = await makeWallet(2, accounts[2])
-    const { testerc20, testerc20d9, testerc721, testerc1155 } = await makeTokens(alice.lastResortAddress)
+    const alice = await CheckUtil.makeWallet(1, accounts[1])
+    const bob = await CheckUtil.makeWallet(2, accounts[2])
+    const { testerc20, testerc20d9, testerc721, testerc1155 } = await CheckUtil.makeTokens(alice.lastResortAddress)
     // transfer ERC20 tokens from alice.lastResortAddress (which owns the tokens) to alices wallet
     await testerc20.transfer(alice.wallet.address, 1000, { from: alice.lastResortAddress })
     // alice transfers tokens to bob
     console.log(`await alice.wallet.getNonce(): ${await alice.wallet.getNonce()}`)
-    await tokenTransfer(
+    await CheckUtil.assetTransfer(
       {
         wallet: alice,
         operationType: ONEConstants.OperationType.TRANSFER_TOKEN,
@@ -337,7 +337,7 @@ contract('ONEWallet', (accounts) => {
     await testerc20d9.transfer(alice.wallet.address, 1000, { from: alice.lastResortAddress })
     // alice transfers tokens to bob
     console.log(`await alice.wallet.getNonce(): ${await alice.wallet.getNonce()}`)
-    await tokenTransfer(
+    await assetTransfer(
       {
         wallet: alice,
         operationType: ONEConstants.OperationType.TRANSFER_TOKEN,
@@ -352,7 +352,7 @@ contract('ONEWallet', (accounts) => {
     assert.equal(alice.wallet.address, await testerc721.ownerOf(8), 'Transfer of ERC721 token 8 to alice.wallet succesful')
     // alice transfers tokens to bob
     console.log(`await alice.wallet.getNonce(): ${await alice.wallet.getNonce()}`)
-    await tokenTransfer(
+    await assetTransfer(
       {
         wallet: alice,
         operationType: ONEConstants.OperationType.TRANSFER_TOKEN,
@@ -368,7 +368,7 @@ contract('ONEWallet', (accounts) => {
     await testerc1155.safeTransferFrom(alice.lastResortAddress, alice.wallet.address, 8, 8, ONEUtil.hexStringToBytes('5'), { from: alice.lastResortAddress })
     // alice transfers tokens to bob
     console.log(`await alice.wallet.getNonce(): ${await alice.wallet.getNonce()}`)
-    await tokenTransfer(
+    await assetTransfer(
       {
         wallet: alice,
         operationType: ONEConstants.OperationType.TRANSFER_TOKEN,
@@ -382,100 +382,3 @@ contract('ONEWallet', (accounts) => {
 */
   })
 })
-
-// makeWallet uses an index and unlocked web3.eth.account and creates and funds a ONEwallet
-const makeWallet = async (accountIndex, lastResortAddress) => {
-  // create wallet
-  const EFFECTIVE_TIME = Math.floor(Date.now() / INTERVAL / 6) * INTERVAL * 6 - DURATION / 2
-  const TEN_ETH = unit.toWei('10', 'ether')
-  const { wallet, seed, hseed, root, client: { layers } } = await TestUtil.createWallet({
-    salt: new BN(accountIndex),
-    effectiveTime: EFFECTIVE_TIME,
-    duration: DURATION,
-    maxOperationsPerInterval: SLOT_SIZE,
-    lastResortAddress,
-    spendingLimit: TEN_ETH
-  })
-  // Fund wallet
-  await web3.eth.sendTransaction({
-    from: lastResortAddress,
-    to: wallet.address,
-    value: TEN_ETH
-  })
-  const state = await CheckUtil.getONEWalletState(wallet)
-  return { wallet, seed, hseed, root, layers, lastResortAddress, state }
-}
-
-// makeTokens makes test ERC20, ERC20Decimals9, ERC721, ERC1155
-const makeTokens = async (owner) => {
-  // create an ERC20
-  const TESTERC20 = artifacts.require('TestERC20')
-  const testerc20 = await TESTERC20.new(10000000, { from: owner })
-  // create an ERC20Decimals9
-  const TESTERC20DECIMALS9 = artifacts.require('TestERC20Decimals9')
-  const testerc20d9 = await TESTERC20DECIMALS9.new(10000000, { from: owner })
-  // create an ERC721
-  const TESTERC721 = artifacts.require('TestERC721')
-  const tids = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-  const uris = ['ipfs://test721/0', 'ipfs://test721/1', 'ipfs://test721/2', 'ipfs://test721/3', 'ipfs://test721/4', 'ipfs://test721/5', 'ipfs://test721/6', 'ipfs://test721/7', 'ipfs://test721/8', 'ipfs://test721/9']
-  const testerc721 = await TESTERC721.new(tids, uris, { from: owner })
-  // create an ERC1155
-  const TESTERC1155 = artifacts.require('TestERC1155')
-  const tids1155 = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-  const amounts1155 = [10, 20, 20, 20, 20, 20, 20, 20, 20, 100]
-  const uris1155 = ['ipfs://test1155/0', 'ipfs://test1155/1', 'ipfs://test1155/2', 'ipfs://test1155/3', 'ipfs://test1155/4', 'ipfs://test1155/5', 'ipfs://test1155/6', 'ipfs://test1155/7', 'ipfs://test1155/8', 'ipfs://test1155/9']
-  const testerc1155 = await TESTERC1155.new(tids1155, amounts1155, uris1155, { from: owner })
-  return { testerc20, testerc20d9, testerc721, testerc1155 }
-}
-
-// tokenTransfer commits and reveals a wallet transaction
-const tokenTransfer = async ({ wallet, operationType, tokenType, contractAddress, tokenId, dest, amount }) => {
-  Debugger.printLayers({ layers: wallet.layers })
-  const otp = ONEUtil.genOTP({ seed: wallet.seed })
-  const index = ONEUtil.timeToIndex({ effectiveTime: EFFECTIVE_TIME })
-  const eotp = await ONE.computeEOTP({ otp, hseed: wallet.hseed })
-  // Format commit and revealParams based on tokenType
-  let commitParams
-  let revealParams
-  let paramsHash
-  switch (operationType) {
-    case ONEConstants.OperationType.TRANSFER:
-      paramsHash = ONEWallet.computeTransferHash
-      commitParams = { dest, amount }
-      revealParams = { dest, amount, operationType }
-      break
-    case ONEConstants.OperationType.TRANSFER_TOKEN:
-      paramsHash = ONEWallet.computeGeneralOperationHash
-      switch (tokenType) {
-        case ONEConstants.TokenType.ERC20:
-          commitParams = { operationType, tokenType, contractAddress, dest, amount }
-          revealParams = { operationType, tokenType, contractAddress, dest, amount }
-          break
-        case ONEConstants.TokenType.ERC721:
-          commitParams = { operationType, tokenType, contractAddress, tokenId, dest, amount }
-          revealParams = { operationType, tokenType, contractAddress, tokenId, dest, amount }
-          break
-        case ONEConstants.TokenType.ERC1155:
-          commitParams = { operationType, tokenType, contractAddress, tokenId, dest, amount }
-          revealParams = { operationType, tokenType, contractAddress, tokenId, dest, amount }
-          break
-        default:
-          console.log(`TODO: add in Token error handling`)
-          return
-      }
-      break
-    default:
-      console.log(`TODO: add in error handling`)
-      return
-  }
-  await TestUtil.commitReveal({
-    Debugger,
-    layers: wallet.layers,
-    index,
-    eotp,
-    paramsHash,
-    commitParams,
-    revealParams,
-    wallet: wallet.wallet
-  })
-}
