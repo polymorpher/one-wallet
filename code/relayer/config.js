f27462ad98ab8789606f25fef2b886cb3b186321
Aaron Li <aaron@qokka.ai>
2022-03-17 21:29:50 -0700

    multi-account relayer, random sampling, and backoff

--- code/relayer/config.js
+++ code/relayer/config.js
@@ -1,4 +1,5 @@
 require('dotenv').config()
+const BN = require('bn.js')
 const DEBUG = process.env['RELAYER_DEBUG'] === 'true' || process.env['RELAYER_DEBUG'] === '1'
 const config = {
   debug: DEBUG,
@@ -11,20 +12,25 @@ const config = {
   },
   corsOrigins: process.env['CORS'],
   secret: process.env['SECRET'],
+  pollingInterval: parseInt(process.env.pollingInterval || 1000),
   networks: {
     'harmony-testnet': {
       key: process.env.HARMONY_TESTNET_KEY || '',
       url: process.env.TESTNET_RPC || 'https://api.s0.b.hmny.io',
       wss: process.env.TESTNET_WSS,
+      mnemonic: process.env.HARMONY_TESTNET_MNEMONIC,
       chainId: 2,
       skip: process.env.SKIP_TESTNET,
+      numAccounts: process.env.TESTNET_NUM_ACCOUNTS || 1,
     },
     'harmony-mainnet': {
       key: process.env.HARMONY_MAINNET_KEY || '',
       url: process.env.MAINNET_RPC || 'https://api.s0.t.hmny.io',
       wss: process.env.MAINNET_WSS,
+      mnemonic: process.env.HARMONY_MAINNET_MNEMONIC,
       chainId: 1,
       skip: process.env.SKIP_MAINNET,
+      numAccounts: process.env.MAINNET_NUM_ACCOUNTS || 1,
     },
     'eth-ganache': {
       url: process.env.GANACHE_RPC || 'http://127.0.0.1:7545',
@@ -32,10 +38,11 @@ const config = {
       key: process.env.ETH_GANACHE_KEY,
       mnemonic: process.env.ETH_GANACHE_MNEMONIC,
       skip: process.env.SKIP_GANACHE,
+      numAccounts: process.env.GANACHE_NUM_ACCOUNTS || 1,
     },
   },
   gasLimit: parseInt(process.env.GAS_LIMIT || '210000'),
-  gasPrice: parseInt(process.env.GAS_PRICE || '200'),
+  gasPrice: new BN(process.env.GAS_PRICE || '200'),
   cache: process.env.CACHE || 'cache'
 }
 module.exports = config
