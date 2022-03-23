524e0807bf63133475ef676212254255f335677e
Aaron Li <aaron@qokka.ai>
2022-03-23 00:05:59 -0700

    working staking ui, stake table and reward display

--- code/client/src/pages/Show/Stake.jsx
+++ code/client/src/pages/Show/Stake.jsx
@@ -1,27 +1,32 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import Button from 'antd/es/button'
 import Col from 'antd/es/col'
 import Row from 'antd/es/row'
-import Typography from 'antd/es/typography'
 import CloseOutlined from '@ant-design/icons/CloseOutlined'
-import { Hint, InputBox, Label, Text, Warning } from '../../components/Text'
+import { Hint, InputBox, Label, Text, Warning, Title, Link } from '../../components/Text'
 import AddressInput from '../../components/AddressInput'
 import { CommitRevealProgress } from '../../components/CommitRevealProgress'
 import AnimatedSection from '../../components/AnimatedSection'
 import util, { autoWalletNameHint } from '../../util'
 import BN from 'bn.js'
 import ShowUtils, { retryUpgrade } from './show-util'
-import { useDispatch, useSelector } from 'react-redux'
+import { useSelector } from 'react-redux'
 import { SmartFlows } from '../../../../lib/api/flow'
 import ONE from '../../../../lib/onewallet'
+import ONEUtil from '../../../../lib/util'
 import ONEConstants from '../../../../lib/constants'
 import { api } from '../../../../lib/api'
 import { Chaining } from '../../api/flow'
-
+import Paths from '../../constants/paths'
 import { OtpStack } from '../../components/OtpStack'
 import { useOps } from '../../components/Common'
 import { useHistory } from 'react-router'
-const { Title, Link } = Typography
+import Divider from 'antd/es/divider'
+import Table from 'antd/es/table'
+import { TallRow } from '../../components/Grid'
+import Space from 'antd/es/space'
+import Spin from 'antd/es/spin'
+import Tooltip from 'antd/es/tooltip'
 
 const Stake = ({
   address,
@@ -30,6 +35,8 @@ const Stake = ({
   prefillAmount, // string, number of tokens, in whole amount (not wei)
   prefillDest, // string, hex format
 }) => {
+  const [delegations, setDelegations] = useState(null)
+  const [reward, setReward] = useState(null)
   const history = useHistory()
   const {
     dispatch, wallet, network, stage, setStage,
@@ -45,7 +52,7 @@ const Stake = ({
 
   const [validatorAddress, setValidatorAddress] = useState({ value: prefillDest || '', label: prefillDest ? util.oneAddress(prefillDest) : '' })
   const [inputAmount, setInputAmount] = useState(prefillAmount || '')
-  console.log(balance, wallet)
+
   const maxSpending = BN.min(new BN(balance), util.getMaxSpending(wallet))
   const { formatted: spendingLimitFormatted } = util.computeBalance(maxSpending.toString(), price)
 
@@ -53,6 +60,16 @@ const Stake = ({
     balance: stakingAmount,
     fiatFormatted: stakingFiatAmountFormatted
   } = util.toBalance(inputAmount || 0, price)
+
+  useEffect(() => {
+    async function init () {
+      const result = await api.staking.getDelegations({ address })
+      setDelegations(result.map((e, i) => ({ ...e, key: `${i}` })))
+      const totalReward = result.map(e => e.reward).reduce((a, b) => a.add(new BN(b)), new BN(0))
+      setReward(util.computeBalance(totalReward, price))
+    }
+    init()
+  }, [])
 
   const useMaxAmount = () => {
     if (new BN(balance, 10).gt(new BN(maxSpending, 10))) {
@@ -97,10 +114,73 @@ const Stake = ({
     })
   }
 
+  const columns = [
+    {
+      title: 'Validator',
+      dataIndex: 'validatorAddress',
+      key: 'validator',
+      // eslint-disable-next-line react/display-name
+      render: (text, record) => {
+        const oneAddress = util.safeOneAddress(record.validatorAddress)
+        return (
+          <Link href={`https://staking.harmony.one/validators/mainnet/${oneAddress}`}>
+            <Tooltip title={oneAddress}>{util.ellipsisAddress(oneAddress)}</Tooltip>
+          </Link>
+        )
+      }
+    }, {
+      title: 'Staked (ONE)',
+      dataIndex: 'amount',
+      key: 'amount',
+      // eslint-disable-next-line react/display-name
+      render: (text, record) => {
+        return ONEUtil.toOne(String(record.amount) || 0)
+      }
+    }, {
+      title: 'Reward (ONE)',
+      dataIndex: 'reward',
+      key: 'reward',
+      // eslint-disable-next-line react/display-name
+      render: (text, record) => {
+        return ONEUtil.toOne(String(record.reward) || 0)
+      }
+    }, {
+      title: 'Action',
+      key: 'action',
+      // eslint-disable-next-line react/display-name
+      render: (text, record) =>
+        (
+          <Button
+            type='primary' size='small' shape='round' onClick={() =>
+              history.push(Paths.showAddress(address, `unstake?validator=${record.validatorAddress}`))}
+          > Undelegate
+          </Button>
+        )
+    }
+  ]
+
+  if (network !== 'harmony-mainnet') {
+    return (
+      <AnimatedSection
+        wide
+        title={
+          <Title level={isMobile ? 5 : 2}>
+            {isMobile ? '' : 'Staking '}
+          </Title>
+      }
+        extra={[
+          <Button key='close' type='text' icon={<CloseOutlined />} onClick={onClose} />
+        ]}
+      >
+        <Warning>Staking is available only on Harmony Mainnet. Please change your network (on top right of the window)</Warning>
+      </AnimatedSection>
+    )
+  }
+
   if (!util.canStake(wallet)) {
     return (
       <AnimatedSection
-        style={{ maxWidth: 720 }}
+        wide
         title={
           <Title level={isMobile ? 5 : 2}>
             {isMobile ? '' : 'Staking '}
@@ -117,7 +197,7 @@ const Stake = ({
 
   return (
     <AnimatedSection
-      style={{ maxWidth: 720 }}
+      wide
       title={
         <Title level={isMobile ? 5 : 2}>
           {isMobile ? '' : 'Staking '}
@@ -128,12 +208,15 @@ const Stake = ({
       ]}
     >
       <Row align='middle' style={{ marginBottom: '32px' }}>
-        <Col>
-          <Text>Stake your ONEs with a validator through delegation. You can find a list of validators <Link href='https://staking.harmony.one/validators/mainnet' target='_blank' rel='noreferrer'>
-            here
-          </Link>. Copy the address of the validator below to stake with them.
-          </Text>
-        </Col>
+        <Text>
+          <Link href='https://docs.harmony.one/home/network/validators/definitions' target='_blank' rel='noreferrer'>Staking</Link> lets you earn rewards (in ONE) over time. You cannot spend the ONEs you staked, until you unstake them (which usually take 7 days).
+          <br /><br />
+        </Text>
+        <Text>You can stake by delegating ONEs to a validator. You can find&nbsp;
+          <Link href='https://staking.harmony.one/validators/mainnet' target='_blank' rel='noreferrer'>
+            a list of validators here
+          </Link> along with their expected rewards. Copy the address of the validator below to stake with them.
+        </Text>
       </Row>
       <Row align='baseline' style={{ marginBottom: '16px' }}>
         <Col xs={4}>
@@ -195,6 +278,35 @@ const Stake = ({
         </Col>
       </Row>
       <CommitRevealProgress stage={stage} />
+      <Divider />
+      <Title level={3}>Your Stakes</Title>
+      <TallRow align='start'>
+        <Col span={isMobile ? 24 : 12}>
+          <Title level={5}>Total accumulated reward</Title>
+        </Col>
+        <Col>
+          <Space direction='vertical' size='large'>
+            <Row>
+              <Space>
+                <Text>{reward ? reward.formatted : <Spin />}</Text>
+                <Text type='secondary'>ONE</Text>
+                <Text>(≈ ${reward ? reward.fiatFormatted : <Spin />}</Text>
+                <Text type='secondary'>USD)</Text>
+              </Space>
+            </Row>
+            <Row>
+              <Space>
+                <Button
+                  type='primary' size='large' shape='round' onClick={() => history.push(Paths.showAddress(address, 'collectStakeReward'))}
+                  disabled={!reward || new BN(reward.balance).eqn(0)}
+                > Collect Reward
+                </Button>
+              </Space>
+            </Row>
+          </Space>
+        </Col>
+      </TallRow>
+      <Table dataSource={delegations} columns={columns} loading={delegations === null} />
     </AnimatedSection>
   )
 }
