8facd2ce2c344cffb70ad938f4def7764b85554c
polymorpher <aaron@qokka.ai>
2022-04-04 02:27:25 -0700

    remove dead code in test/admin

--- code/test/admin.js
+++ code/test/admin.js
@@ -1,23 +1,10 @@
 // Administrative Function Testing
-
 const TestUtil = require('./util')
-// const unit = require('ethjs-unit')
-// const ONEUtil = require('../lib/util')
-// const ONEConstants = require('../lib/constants')
-// const BN = require('bn.js')
-
-// const ONE_CENT = unit.toWei('0.01', 'ether')
-// const HALF_DIME = unit.toWei('0.05', 'ether')
-// const ONE_DIME = unit.toWei('0.1', 'ether')
-// const HALF_ETH = unit.toWei('0.5', 'ether')
 const INTERVAL = 30000 // 30 second Intervals
 const DURATION = INTERVAL * 12 // 6 minute wallet duration
-// const SLOT_SIZE = 1 // 1 transaction per interval
+// Wallets effective time is the current time minus half the duration (3 minutes ago)
 const EFFECTIVE_TIME = Math.floor(Date.now() / INTERVAL / 6) * INTERVAL * 6 - DURATION / 2
-
-// === TESTING
 contract('ONEWallet', (accounts) => {
-  // Wallets effective time is the current time minus half the duration (3 minutes ago)
   let snapshotId
   beforeEach(async function () {
     snapshotId = await TestUtil.snapshot()
