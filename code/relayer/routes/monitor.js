d6edc1e51a37eecea26b82d62b55ebe8ac6a75b8
polymorpher <aaron@qokka.ai>
2022-04-16 02:42:45 -0700

    basic monitor route

---
+++ code/relayer/routes/monitor.js
@@ -0,0 +1,28 @@
+const config = require('../config')
+const express = require('express')
+const router = express.Router()
+const { StatusCodes } = require('http-status-codes')
+const { rpc } = require('../rpc')
+
+router.get('/block', async (req, res) => {
+  const network = req.header('X-NETWORK') || config.defaultNetwork
+  if (!config.networks[network]) {
+    return res.status(StatusCodes.BAD_REQUEST).json({ network, error: 'invalid network' })
+  }
+  const pall = Promise.all([
+    rpc.latestHeader({ network, beacon: true }),
+    rpc.latestHeader({ network, beacon: false })
+  ])
+  const timeout = new Promise((resolve) => setTimeout(() => resolve([null, null]), 1000))
+  let blockDiff = null
+  try {
+    const [h1, h2] = await Promise.race([pall, timeout])
+    blockDiff = parseInt(h1.blockNumber) - parseInt(h2.blockNumber)
+  } catch (ex) {
+    console.error(ex)
+  }
+  return res.json({
+    blockDiff,
+    timeDiff: blockDiff * config.networks[network].blockTime,
+  })
+})
