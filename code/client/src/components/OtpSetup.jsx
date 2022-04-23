412a7afea923f71bbb03e24fdc1f9e6f6e17c7fc
Tao Zhou <iamzhoutao92@gmail.com>
2022-04-23 22:51:45 +0200

    v2ui of Create flow (part 1)

--- code/client/src/components/OtpSetup.jsx
+++ code/client/src/components/OtpSetup.jsx
@@ -3,6 +3,7 @@ import OtpBox from './OtpBox'
 import Button from 'antd/es/button'
 import Checkbox from 'antd/es/checkbox'
 import Space from 'antd/es/space'
+import Row from 'antd/es/row'
 import Tooltip from 'antd/es/tooltip'
 import QuestionCircleOutlined from '@ant-design/icons/QuestionCircleOutlined'
 import SnippetsOutlined from '@ant-design/icons/SnippetsOutlined'
@@ -27,6 +28,27 @@ export const OtpSetup = ({ isMobile, otpRef, otpValue, setOtpValue, name }) => {
   )
 }
 
+export const OtpSetup2 = ({ isMobile, otpRef, otpValue, setOtpValue, name }) => {
+  return (
+    <Row align='center' style={{ width: '100%', alignItems: 'center' }}>
+      <div style={{ marginRight: '16px', display: 'flex', flexDirection: 'column' }}>
+        <div style={{ margin: 0 }}>To create, enter Authentication Code</div>
+        <div style={{ fontSize: 'smaller' }}>Harmony ({name})</div>
+      </div>
+      <OtpBox
+        shouldAutoFocus={!isMobile}
+        ref={otpRef}
+        value={otpValue}
+        onChange={setOtpValue}
+        numOnly={isMobile}
+        containerStyle={{ minWidth: '300px' }}
+        inputStyle={{ height: '60px', color: 'black' }}
+      />
+      {isMobile && <Button type='default' shape='round' icon={<SnippetsOutlined />} onClick={() => { navigator.clipboard.readText().then(t => setOtpValue(t)) }}>Paste from Clipboard</Button>}
+    </Row>
+  )
+}
+
 export const TwoCodeOption = ({ doubleOtp, setDoubleOtp, isMobile }) => {
   return (
     <Checkbox onChange={() => setDoubleOtp(!doubleOtp)}>
@@ -38,6 +60,21 @@ export const TwoCodeOption = ({ doubleOtp, setDoubleOtp, isMobile }) => {
           <QuestionCircleOutlined />
         </Tooltip>
       </Space>
+    </Checkbox>
+  )
+}
+
+export const TwoCodeOption2 = ({ doubleOtp, setDoubleOtp }) => {
+  return (
+    <Checkbox onChange={() => setDoubleOtp(!doubleOtp)} style={{ color: 'inherit' }}>
+      <Row style={{ alignItems: 'center', justifyContent: 'center' }}>
+        <Hint style={{ fontSize: '14px', color: 'inherit' }}>
+          Use two codes to enhance security
+        </Hint>
+        <Tooltip title={<div>You will need to scan another QR-code on the next page. Each time you make a transaction, you will need to type in two 6-digit codes, which are shown simultaneously next to each other on your Google Authenticator.<br /><br />This is advisable if you intend to make larger transactions with this wallet</div>}>
+          <QuestionCircleOutlined style={{ marginLeft: '5px' }} />
+        </Tooltip>
+      </Row>
     </Checkbox>
   )
 }
