5f687854f0659c3c0222e59d56859f01bd17252b
polymorpher <aaron@qokka.ai>
2022-04-28 10:35:38 -0700

    fix stats bugs

--- code/scripts/stats.js
+++ code/scripts/stats.js
@@ -12,11 +12,13 @@ initBlockchain()
 const moment = require('moment-timezone')
 const T0 = process.env.T0 ? Date.parse(process.env.T0) : Date.now() - 3600 * 1000 * 24 * 3
 const RELAYER_ADDRESSES = (process.env.RELAYER_ADDRESSES || '0xc8cd0c9ca68b853f73917c36e9276770a8d8e4e0').split(',').map(s => s.toLowerCase().trim())
-const STATS_CACHE = process.env.OUT || './data/stats.json'
-const ADDRESSES_OUT = process.env.OUT || './data/addresses.csv'
+const STATS_CACHE = process.env.STATS_CACHE || './data/stats.json'
+const ADDRESSES_CACHE = process.env.ADDRESSES_CACHE || './data/addresses.csv'
 const MAX_BALANCE_AGE = parseInt(process.env.MAX_BALANCE_AGE || 3600 * 1000 * 24)
 const SLEEP_BETWEEN_RPC = parseInt(process.env.SLEEP_BETWEEN_RPC || 100)
 const RPC_BATCH_SIZE = parseInt(process.env.RPC_BATCH_SIZE || 50)
+const PAGE_SIZE = parseInt(process.env.PAGE_SIZE || 500)
+
 const computeDirectCreationContractAddress = (from, nonce) => {
   const encoded = new Uint8Array(rlp.encode([from, nonce]))
   const hashed = ONEUtil.keccak(encoded)
@@ -43,7 +45,7 @@ const search = async ({ address, target }) => {
   let left = 0; let mid = 1; let right = -1
   while (right < 0 || (left + 1 < right && left !== mid)) {
     console.log(`Binary searching pageIndex`, { left, mid, right })
-    const transactions = await api.rpc.getTransactionHistory({ address, pageIndex: mid, pageSize: 500, fullTx: false })
+    const transactions = await api.rpc.getTransactionHistory({ address, pageIndex: mid, pageSize: PAGE_SIZE, fullTx: false })
     const h = transactions[transactions.length - 1]
     if (!h) {
       right = mid
@@ -67,22 +69,22 @@ const search = async ({ address, target }) => {
   return left
 }
 // TODO: add a binary search function to jump pageIndex to <to>
-const scan = async ({ address, from = T0, to = Date.now(), updateBalance = false }) => {
+const scan = async ({ address, from = T0, to = Date.now(), retrieveBalance = true }) => {
   let pageIndex = await search({ address, target: to })
   let tMin = to
   console.log({ from, to })
   const wallets = []
   while (tMin > from) {
-    const transactions = await api.rpc.getTransactionHistory({ address, pageIndex, pageSize: 500, fullTx: true })
+    const transactions = await api.rpc.getTransactionHistory({ address, pageIndex, pageSize: PAGE_SIZE, fullTx: true })
     if (!transactions || transactions.length === 0) {
-      console.log(`Out of data at page ${pageIndex}; Exiting`)
+      console.log(`Out of data at page ${pageIndex}; Exiting transaction history query loop`)
       tMin = from
       break
     }
     // console.log(transactions)
     tMin = Math.min(tMin, min(transactions.map(t => new BN(t.timestamp.slice(2), 16).toNumber() * 1000)))
     const creations = transactions.filter(e => e.input.startsWith('0x60806040'))
-    console.log(`searched up to time = ${timeString(tMin)}; at page ${pageIndex}; retrieved ${transactions.length} transactions from relayer; ${creations.length} creations of 1wallet`)
+    console.log(`Searched transaction history down to time = ${timeString(tMin)}; at page ${pageIndex}; retrieved ${transactions.length} transactions from relayer; ${creations.length} creations of 1wallet`)
 
     creations.forEach((t) => {
       const { timestamp, nonce } = t
@@ -96,21 +98,21 @@ const scan = async ({ address, from = T0, to = Date.now(), updateBalance = false
     await new Promise((resolve) => setTimeout(resolve, SLEEP_BETWEEN_RPC))
   }
   const uniqueWallets = uniqBy(wallets, w => w.address)
-  const balances = updateBalance && await batchGetBalance(uniqueWallets.map(w => w.address))
+  const balances = retrieveBalance && await batchGetBalance(uniqueWallets.map(w => w.address))
   return { balances, wallets: uniqueWallets }
 }
 
 async function exec () {
   const fp = await fs.open(STATS_CACHE, 'a+')
-  const fp2 = await fs.open(ADDRESSES_OUT, 'w+')
+  const fp2 = await fs.open(ADDRESSES_CACHE, 'w+')
   const stats = JSON.parse((await fs.readFile(STATS_CACHE, { encoding: 'utf-8' }) || '{}'))
   const now = Date.now()
   const updateBalance = now - (stats.lastBalanceUpdate || 0) >= MAX_BALANCE_AGE
   const from = stats.lastScanTime || 0
-  let totalBalance = new BN(0)
-  let totalAddresses = 0
+  let totalBalance = new BN(stats.totalBalance)
+  let totalAddresses = stats.totalAddresses
   for (const address of RELAYER_ADDRESSES) {
-    const { balances, wallets } = await scan({ address, from, updateBalance })
+    const { balances, wallets } = await scan({ address, from })
     totalAddresses += wallets.length
     if (balances) {
       totalBalance = totalBalance.add(balances.reduce((r, b) => r.add(new BN(b)), new BN(0)))
